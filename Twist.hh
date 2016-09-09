3929468e9f8734cff689e8f313e4b1a6eb295487
Bastian Rieck <bastian@rieck.ru>
2016-09-09 09:40:14 +0200

    Implemented twist reduction
    Also did some ad-hoc testing, but the simple triangle data set is not
    expressive enough to exploit the column clearing operation.

---
+++ Twist.hh
@@ -0,0 +1,54 @@
+#ifndef ALEPH_TWIST_HH__
+#define ALEPH_TWIST_HH__
+
+#include "BoundaryMatrix.hh"
+
+#include <tuple>
+#include <vector>
+
+namespace aleph
+{
+
+class TwistReduction
+{
+public:
+  template <class Representation> void operator()( BoundaryMatrix<Representation>& M )
+  {
+    using Index = typename Representation::Index;
+
+    auto dimension  = M.getDimension();
+    auto numColumns = M.getNumColumns();
+
+    std::vector< std::pair<Index, bool> > lut( numColumns,
+                                               std::make_pair(0, false) );
+
+    for( Index d = dimension; d >= 1; d-- )
+    {
+      for( Index j = 0; j < numColumns; j++ )
+      {
+        if( M.getDimension( j ) == d )
+        {
+          Index i;
+          bool valid = false;
+
+          std::tie( i, valid ) = M.getMaximumIndex( j );
+          while( valid && lut[i].second )
+          {
+            M.addColumns( lut[i].first, j );
+            std::tie( i, valid ) = M.getMaximumIndex( j );
+          }
+
+          if( valid )
+          {
+            lut[i] = std::make_pair( j, true );
+            M.clearColumn( i );
+          }
+        }
+      }
+    }
+  }
+};
+
+}
+
+#endif
--- aleph_test.cc
+++ aleph_test.cc
@@ -2,6 +2,7 @@
 #include "Vector.hh"
 #include "IO.hh"
 #include "Standard.hh"
+#include "Twist.hh"
 #include "PersistencePairs.hh"
 
 #include <iostream>
@@ -13,6 +14,7 @@ using I  = unsigned;
 using V  = Vector<I>;
 using BM = BoundaryMatrix<V>;
 using SR = StandardReduction;
+using TR = TwistReduction;
 
 int main()
 {
@@ -22,4 +24,5 @@ int main()
             << "* Maximum dimension: " << M.getDimension() << "\n";
 
   computePersistencePairs<SR>( M );
+  computePersistencePairs<TR>( M );
 }
