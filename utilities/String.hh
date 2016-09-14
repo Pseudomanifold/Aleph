f20e0aa7006649d8ad5ed36ff60e3ca1bf1ecd97
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2016-09-14 10:01:25 +0200

    Started implementing some string utility functions
    I will inevitably need them when attempting to parse more complex data
    files. Although I could use Boost for this as well, I want to keep the
    interface as simple as possible.

---
+++ utilities/String.hh
@@ -0,0 +1,48 @@
+#ifndef ALEPH_STRING_HH__
+#define ALEPH_STRING_HH__
+
+#include <algorithm>
+
+#include <cctype>
+
+namespace aleph
+{
+
+namespace utilities
+{
+
+template <class T> T ltrim( T sequence )
+{
+  sequence.erase( sequence.begin(),
+                  std::find_if_not( sequence.begin(), sequence.end(),
+                                    [] ( const typename T::value_type& c )
+                                    {
+                                      return std::isspace( c );
+                                    } ) );
+
+  return sequence;
+}
+
+template <class T> T rtrim( T sequence )
+{
+  sequence.erase( std::find_if_not( sequence.rbegin(), sequence.rend(),
+                                    [] ( const typename T::value_type& c )
+                                    {
+                                      return std::isspace( c );
+                                    } ).base(),
+                  sequence.end() );
+
+  return sequence;
+}
+
+template <class T> T trim( T sequence )
+{
+  return ltrim( rtrim( sequence ) );
+}
+
+
+}
+
+}
+
+#endif
