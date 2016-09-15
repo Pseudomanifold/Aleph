eccecd5f3e75ba17a6ded39d1368326d98bf9897
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2016-09-15 10:06:59 +0200

    Added new list representation
    This one *may* be useful when contiguous memory regions are not
    available. However, it is of course outperformed by the other
    representations.

---
+++ representations/List.hh
@@ -0,0 +1,103 @@
+#ifndef ALEPH_REPRESENTATIONS_LIST_HH__
+#define ALEPH_REPRESENTATIONS_LIST_HH__
+
+#include <algorithm>
+#include <list>
+#include <utility>
+#include <vector>
+
+namespace aleph
+{
+
+namespace representations
+{
+
+template <class IndexType = unsigned> class List
+{
+public:
+  using Index = IndexType;
+
+  void setNumColumns( Index numColumns )
+  {
+    _data.resize( numColumns );
+    _dimensions.resize( numColumns );
+  }
+
+  Index getNumColumns() const
+  {
+    return static_cast<Index>( _data.size() );
+  }
+
+  std::pair<Index, bool> getMaximumIndex( Index column ) const
+  {
+    if( _data.at( column ).empty() )
+      return std::make_pair( Index(0), false );
+    else
+      return std::make_pair( _data.at( column ).back(), true );
+  }
+
+  void addColumns( Index source, Index target )
+  {
+    auto&& sourceColumn = _data.at( source );
+    auto&& targetColumn = _data.at( target );
+
+    std::list<Index> result;
+
+    std::set_symmetric_difference( sourceColumn.begin(), sourceColumn.end(),
+                                   targetColumn.begin(), targetColumn.end(),
+                                   std::back_inserter( result ) );
+
+    targetColumn.swap( result );
+  }
+
+  template <class InputIterator> void setColumn( Index column,
+                                                 InputIterator begin, InputIterator end )
+  {
+    _data.at( column ).assign( begin, end );
+
+    // Ensures proper sorting order. Else, the reduction algorithm will
+    // not be able to reduce the matrix.
+    _data.at( column ).sort();
+
+    // Upon initialization, the column must by necessity have the dimension
+    // that is indicated by the amount of indices in its boundary. The case
+    // of 0-simplices needs special handling.
+    _dimensions.at( column )
+        = begin == end ? 0
+                       : static_cast<Index>( std::distance( begin, end ) - 1 );
+  }
+
+  std::vector<Index> getColumn( Index column ) const
+  {
+    return { _data.at( column ).begin(), _data.at( column ).end() };
+  }
+
+  void clearColumn( Index column )
+  {
+    _data.at( column ).clear();
+  }
+
+  Index getDimension( Index column ) const
+  {
+    return _dimensions.at( column );
+  }
+
+  Index getDimension() const
+  {
+    if( _dimensions.empty() )
+      return Index(0);
+    else
+      return *std::max_element( _dimensions.begin(), _dimensions.end() );
+  }
+
+private:
+  std::vector< std::list<Index> > _data;
+  std::vector<Index> _dimensions;
+};
+
+}
+
+}
+
+
+#endif
