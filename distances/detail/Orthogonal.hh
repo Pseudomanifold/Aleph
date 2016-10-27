5feae0a4bd1d9223e6a3bdffaa5f41e7c6d4eb92
Bastian Rieck <bastian@rieck.ru>
2016-10-27 11:11:42 -0400

    Added distance to orthogonal projection

---
+++ distances/detail/Orthogonal.hh
@@ -0,0 +1,34 @@
+#ifndef ALEPH_DISTANCES_DETAIL_ORTHOGONAL_HH__
+#define ALEPH_DISTANCES_DETAIL_ORTHOGONAL_HH__
+
+namespace aleph
+{
+
+namespace distances
+{
+
+namespace detail
+{
+
+template <class Distance, class Point> auto orthogonalDistance( const Point& p ) -> decltype( Distance::operator()( p,p ) )
+{
+  using DataType = decltype( Distance::operator()( p,p ) );
+
+  auto&& x = p.x();
+  auto&& y = p.y();
+
+  // Here's to hoping that the division by two is not being truncated by the
+  // underlying data type. If it is, there's nothing we can do...
+  auto&& u = ( x + y ) / 2;
+
+  Point q( u,u );
+  return Distance().operator()( p,q );
+}
+
+}
+
+}
+
+}
+
+#endif
