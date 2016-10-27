2c19bde07b6e08d497d1fb57bca157cd25854cd9
Bastian Rieck <bastian@rieck.ru>
2016-10-27 11:05:39 -0400

    Extended Wasserstein distance tests
    They are still failing right now because I am not yet handling the
    orthogonal projection correctly.

--- tests/test_wasserstein.cc
+++ tests/test_wasserstein.cc
@@ -18,15 +18,31 @@ int main()
   D1.add( 2.9, 3.0 );
   D1.add( 3.9, 4.0 );
 
+  {
+    auto d11 = wassersteinDistance( D1, D1 );
+
+    assert( d11 >= DataType() );
+    assert( d11 == DataType() );
+  }
+
   Diagram D2;
   D2.add( 0.9, 1.0 );
   D2.add( 1.9, 2.0 );
   D2.add( 2.9, 3.0 );
-  D2.add( 3.9, 4.0 );
+  D2.add( 3.9, 9.9 );
 
-  auto d11 = wassersteinDistance( D1, D1 );
-  auto d12 = wassersteinDistance( D1, D2 );
+  {
+    auto d12 = wassersteinDistance( D1, D2 );
+    auto d21 = wassersteinDistance( D2, D1 );
 
-  std::cerr << "d11 = " << d11 << std::endl
-            << "d12 = " << d12 << std::endl;
+    assert( d12 >= DataType() );
+    assert( d21 >= DataType() );
+
+    assert( d12 == d21 );
+
+    std::cerr << "d12 = " << d12 << std::endl;
+    std::cerr << "d21 = " << d21 << std::endl;
+
+    assert( d12 == DataType( 3.0 ) );
+  }
 }
