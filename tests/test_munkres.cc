903b768de8ee40d2cd8a9666cc2bda2f75fa1c07
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2016-11-07 09:38:37 +0100

    Extended test case for Hungarian algorithm

--- tests/test_munkres.cc
+++ tests/test_munkres.cc
@@ -29,8 +29,38 @@ template <class T> void threeByThree()
   ALEPH_TEST_END();
 }
 
+template <class T> void fourByFour()
+{
+  ALEPH_TEST_BEGIN( "Solving a four-by-four matrix" );
+
+  Matrix<T> m( 4 );
+
+  m(0,0) = static_cast<T>(82); m(0,1) = static_cast<T>(83); m(0,2) = static_cast<T>(69); m(0,3) = static_cast<T>(92);
+  m(1,0) = static_cast<T>(77); m(1,1) = static_cast<T>(37); m(1,2) = static_cast<T>(49); m(1,3) = static_cast<T>(92);
+  m(2,0) = static_cast<T>(11); m(2,1) = static_cast<T>(69); m(2,2) = static_cast<T>( 5); m(2,3) = static_cast<T>(86);
+  m(3,0) = static_cast<T>( 8); m(3,1) = static_cast<T>( 9); m(3,2) = static_cast<T>(98); m(3,3) = static_cast<T>(23);
+
+  Munkres<T> solver( m );
+  solver();
+
+  auto cost = solver.cost( m );
+
+  ALEPH_ASSERT_THROW( cost  > T()   );
+  ALEPH_ASSERT_THROW( cost == T(140) );
+
+  // TODO: Check...
+  // 2,0
+  // 1,1
+  // 0,2
+  // 3,3
+
+  ALEPH_TEST_END();
+}
+
 int main()
 {
+  // 3x3 ---------------------------------------------------------------
+
   threeByThree<short>         ();
   threeByThree<unsigned short>();
   threeByThree<int>           ();
@@ -40,28 +70,15 @@ int main()
   threeByThree<float>         ();
   threeByThree<double>        ();
 
-#if 0
+  // 4x4 ---------------------------------------------------------------
 
-  {
-    Matrix<unsigned short> m( 4 );
+  fourByFour<short>         ();
+  fourByFour<unsigned short>();
+  fourByFour<int>           ();
+  fourByFour<unsigned int>  ();
+  fourByFour<long>          ();
+  fourByFour<unsigned long> ();
+  fourByFour<float>         ();
+  fourByFour<double>        ();
 
-    m(0,0) = 82; m(0,1) = 83; m(0,2) = 69; m(0,3) = 92;
-    m(1,0) = 77; m(1,1) = 37; m(1,2) = 49; m(1,3) = 92;
-    m(2,0) = 11; m(2,1) = 69; m(2,2) =  5; m(2,3) = 86;
-    m(3,0) =  8; m(3,1) =  9; m(3,2) = 98; m(3,3) = 23;
-
-    Munkres<unsigned short> solver( m );
-    std::cerr << solver();
-
-    std::cerr << solver.cost( m ) << "\n";
-
-    // TODO: Check...
-    // 2,0
-    // 1,1
-    // 0,2
-    // 3,3
-    // Costs should be 140
-  }
-
-#endif
 }
