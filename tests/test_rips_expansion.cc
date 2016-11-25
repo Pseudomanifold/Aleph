090bce4a82629928f8f240a6e99b15f0d701d315
Bastian Rieck <bastian@rieck.ru>
2016-11-25 12:48:01 +0100

    Prepared test for triangle Rips expansion

--- tests/test_rips_expansion.cc
+++ tests/test_rips_expansion.cc
@@ -1,5 +1,37 @@
 #include "complexes/RipsExpander.hh"
 
+#include "tests/Base.hh"
+
+#include "Simplex.hh"
+#include "SimplicialComplex.hh"
+
+#include <vector>
+
+using namespace aleph::complexes;
+using namespace aleph;
+
+template <class Data, class Vertex> bool triangle()
+{
+  ALEPH_TEST_BEGIN( "Triangle" );
+
+  using Simplex           = Simplex<Data, Vertex>;
+  using SimplicialComplex = SimplicialComplex<Simplex>;
+
+  std::vector<Simplex> simplices
+    = { {0}, {1}, {2}, {0,1}, {0,2}, {1,2} };
+
+  SimplicialComplex K( simplices.begin(), simplices.end() );
+  RipsExpander<Simplex> ripsExpander;
+
+  auto vr = ripsExpander( K, 2 );
+
+  ALEPH_ASSERT_THROW( vr.empty() == false );
+
+  ALEPH_TEST_END();
+  return true;
+}
+
 int main()
 {
+  triangle<double, unsigned>();
 }
