9a01b487de0bdf73e9f2457347d900e639d03aef
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2016-11-29 11:01:19 +0100

    Added network analysis example

---
+++ examples/network_analysis.cc
@@ -0,0 +1,52 @@
+#include "filtrations/Data.hh"
+
+#include "io/EdgeLists.hh"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "Simplex.hh"
+#include "SimplicialComplex.hh"
+
+#include "PersistentHomologyCalculation.hh"
+
+using DataType          = double;
+using VertexType        = unsigned;
+
+using Simplex           = aleph::Simplex<DataType, VertexType>;
+using SimplicialComplex = aleph::SimplicialComplex<Simplex>;
+
+int main( int argc, char** argv )
+{
+  if( argc <= 1 )
+    return -1;
+
+  aleph::io::EdgeListReader reader;
+
+  std::string input = argv[1];
+  std::ifstream in( input );
+
+  std::cerr << "* Reading '" << input << "'...";
+
+  SimplicialComplex K;
+  reader( in, K );
+
+  std::cerr << "finished\n"
+            << "* Extracted simplicial complex with " << K.size() << " simplices\n";
+
+  std::cerr << "* Establishing filtration order...";
+
+  K.sort(
+    aleph::filtrations::Data<Simplex>()
+  );
+
+  std::cerr << "...finished\n";
+
+  std::cerr << "* Calculating persistent homology...";
+
+  auto diagrams
+    = calculatePersistenceDiagrams( K );
+
+  std::cerr << "...finished\n";
+}
