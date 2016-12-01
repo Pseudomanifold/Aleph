6759db0a8957e6049d215ca2d0ed36fa1dbe9cd0
Bastian Rieck <bastian@rieck.ru>
2016-12-01 13:28:42 +0100

    Euclidean distance functor for neighbourhood graph expansion

---
+++ distances/Euclidean.hh
@@ -0,0 +1,132 @@
+#ifndef ALEPH_DISTANCES_EUCLIDEAN_HH__
+#define ALEPH_DISTANCES_EUCLIDEAN_HH__
+
+#include <cmath>
+#include <cstddef>
+
+#include <iterator>
+#include <string>
+
+namespace aleph
+{
+
+namespace distances
+{
+
+/**
+  Euclidean distance ($L_2$ distance) functor. The functor is templated in
+  order to handle arbitrary ranges. Its interface is somewhat general, but
+  the \c accum_dist is specifically meant to be used with \c FLANN.
+
+  @see FLANN project page (http://www.cs.ubc.ca/research/flann)
+  @see FLANN repository   (https://github.com/mariusmuja/flann)
+*/
+
+class Euclidean
+{
+public:
+
+  // Flag telling FLANN that this functor can be used for calculating distances
+  // within kd trees.
+  typedef bool is_kdtree_distance;
+
+  // Required for FLANN usage
+  using ElementType = double;
+  using ResultType  = double;
+
+  /**
+    Given two ranges of double values, which are assumed to represent two
+    vectors, calculates the distance between them.
+
+    @param a             Iterator describing first vector
+    @param b             Iterator describing second vector
+    @param size          Size of vectors a and b
+
+    @param worstDistance If set to a value greater than zero, calculations will
+    stop once the value has been reached. Else, the value of this variable is
+    ignored. This variable is provided in order to remain compatible with the
+    interface of FLANN.
+  */
+
+  template <typename Iterator1, typename Iterator2>
+  ResultType operator()( Iterator1 a,
+                         Iterator2 b,
+                         std::size_t size,
+                         ElementType worstDistance = -1.0 ) const
+  {
+    // Fix compiler warnings about unused parameters. This is provided to be
+    // compatible with FLANN.
+    (void) worstDistance;
+
+    ResultType result = 0.0;
+
+    ResultType diff0  = 0.0;
+    ResultType diff1  = 0.0;
+    ResultType diff2  = 0.0;
+    ResultType diff3  = 0.0;
+
+    Iterator1 last      = a; 
+    Iterator1 lastGroup = last;
+
+    std::advance( last,     size );
+    std::advance( lastGroup, -3 );
+
+    while( a < lastGroup )
+    {
+      diff0 = std::abs( ResultType( a[0] - b[0] ) );
+      diff1 = std::abs( ResultType( a[1] - b[1] ) );
+      diff2 = std::abs( ResultType( a[2] - b[2] ) );
+      diff3 = std::abs( ResultType( a[3] - b[3] ) );
+
+      result +=   diff0 * diff0
+                + diff1 * diff1
+                + diff2 * diff2
+                + diff3 * diff3;
+
+      a += 4;
+      b += 4;
+
+      if( worstDistance > 0 && result > worstDistance )
+        return result;
+    }
+
+    while( a < last )
+    {
+      diff0  = std::abs( ResultType( *a++ - *b++ ) );
+      result = result + diff0 * diff0;
+    }
+
+    return result;
+  }
+
+  /**
+    Partial distance calculation, used by FLANN for fast kd-tree calculations.
+    This function exploits that the Euclidean distance can be evaluated
+    component-wise.
+
+    @param a First component
+    @param b Second component
+
+    @returns Partial distance between those two components
+  */
+
+  template <typename U, typename V>
+  ResultType accum_dist( const U& a,
+                         const V& b,
+                         int __attribute__((unused)) ) const
+  {
+    return (a-b) * (a-b);
+  }
+
+  /** @returns Name of functor */
+  static std::string name()
+  {
+    return "Euclidean distance";
+  }
+};
+
+}
+
+}
+
+#endif
