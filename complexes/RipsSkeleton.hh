b8ad9c7665347b2f9a11663715c699d5c77573ac
Bastian Rieck <bastian@rieck.ru>
2016-12-01 16:14:26 +0100

    1-skeleton calculation for Rips skeleton
    Still untested, but this should now permit me to calculate Rips
    skeletons---and thus Vietoris--Rips complexes---from almost any
    point cloud, provided I can assign it useful distances.

--- complexes/RipsSkeleton.hh
+++ complexes/RipsSkeleton.hh
@@ -31,6 +31,27 @@ public:
     for( decltype(numVertices) i = 0; i < numVertices; i++ )
       simplices.push_back( Simplex( i ) );
 
+    std::vector< std::vector<IndexType> > indices;
+    std::vector< std::vector<ElementType> > distances;
+
+    nn.radiusSearch( epsilon, indices, distances );
+
+    for( std::size_t i = 0; i < indices.size(); i++ )
+    {
+      IndexType u = IndexType(i);
+
+      for( std::size_t j = 0; j < indices[i].size(); j++ )
+      {
+        IndexType v   = IndexType( indices[i][j] );
+        ElementType d = distances[i][j];
+
+        // Ensures that edges are only added once. Otherwise, edge u--v will
+        // also give rise to v--u.
+        if( u < v )
+          simplices.push_back( Simplex( {u,v}, d ) );
+      }
+    }
+
     return SimplicialComplex( simplices.begin(), simplices.end() );
   };
 };
