a43a97be5e91a653d756373d1e44bb8ed1c26731
Bastian Rieck <bastian@rieck.ru>
2016-12-02 20:41:43 +0100

    Improved documentation

--- complexes/VietorisRipsComplex.hh
+++ complexes/VietorisRipsComplex.hh
@@ -20,6 +20,13 @@ namespace geometry
 
   Note that this function does not give you the opportunity to try out another
   class than the default simplicial complex and simplex class.
+
+  The resulting simplicial complex will use the standard weight function, i.e.
+  a simplex has a weight that is equal to the maximum weight of its faces. The
+  0-simplices have a weight of 0. 1-simplices use the distance between the two
+  vertices of the corresponding edge as a weight.
+
+  Hence, this complex fully represents the scale of the distance function.
 */
 
 template <class NearestNeighbours> auto buildVietorisRipsComplex(
@@ -29,15 +36,15 @@ template <class NearestNeighbours> auto buildVietorisRipsComplex(
 {
   using ElementType       = typename NearestNeighbours::ElementType;
   using IndexType         = typename NearestNeighbours::IndexType;
-  using Simplex           = Simplex<Element, IndexType>;
+  using Simplex           = Simplex<ElementType, IndexType>;
   using SimplicialComplex = SimplicialComplex<Simplex>;
 
-  RipsSkeleton<NearestNeighbours> ripsSkeleton;
+  complexes::RipsSkeleton<NearestNeighbours> ripsSkeleton;
 
   auto skeleton
     = ripsSkeleton.build( nn, epsilon );
 
-  RipsExpander<SimplicialComplex> ripsExpander;
+  complexes::RipsExpander<SimplicialComplex> ripsExpander;
 
   auto K = ripsExpander( skeleton, dimension );
   K      = ripsExpander.assignMaximumWeight( K );
