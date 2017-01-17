2a2c03b9fd40d4c88c43aaa2607fb1ba39d70cdf
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-01-17 10:30:32 +0100

    Implemented truncated Gaussian density estimator

--- containers/DataDescriptors.hh
+++ containers/DataDescriptors.hh
@@ -6,6 +6,7 @@
 #include <numeric>
 #include <vector>
 
+#include "distances/Euclidean.hh"
 #include "distances/Traits.hh"
 
 namespace aleph
@@ -57,6 +58,45 @@ template <class Distance, class Container> std::vector<double> eccentricities( c
   return eccentricities;
 }
 
+/**
+  Density estimation using a truncated Gaussian estimator. Points whose
+  Euclidean distance is smaller than the bandwidth will not be used for
+  estimating the density.
+*/
+
+template <class Container> std::vector<double> estimateDensityTruncatedGaussian( const Container& container, double bandwidth )
+{
+  auto n = container.size();
+  auto d = container.dimension();
+
+  const auto bandwidthSquare = bandwidth * bandwidth;
+
+  std::vector<double> densities;
+  densities.reserve( n );
+
+  distances::Euclidean<double> distanceFunctor;
+
+  for( decltype(n) i = 0; i < n; i++ )
+  {
+    auto p       = container[i];
+    auto density = 0.0;
+
+    for( decltype(n) j = 0; j < n; j++ )
+    {
+      auto q        = container[j];
+      auto distance = distanceFunctor( p.begin(),
+                                       q.begin(),
+                                       d );
+      if( distance <= bandwidthSquare )
+        density += std::exp( -1.0 * distance / ( 2.0 * bandwidth ) );
+    }
+
+    densities.push_back( density / static_cast<double>(n) );
+  }
+
+  return densities;
 }
+
+} // namespace aleph
 
 #endif
