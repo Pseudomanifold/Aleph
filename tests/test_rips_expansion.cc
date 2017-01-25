583ea389d8f3f37db357fab9583d47357510c3ec
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-01-25 10:49:31 +0100

    Started extending test case for Rips expansion
    Need to ensure that the weight assignment process works as expected
    because I encountered some problems with it for complexes of higher
    dimensionality.

--- tests/test_rips_expansion.cc
+++ tests/test_rips_expansion.cc
@@ -7,11 +7,13 @@
 
 #include <vector>
 
+#include <cmath>
+
 using namespace aleph::geometry;
 using namespace aleph::topology;
 using namespace aleph;
 
-template <class Data, class Vertex> bool triangle()
+template <class Data, class Vertex> void triangle()
 {
   ALEPH_TEST_BEGIN( "Triangle" );
 
@@ -33,10 +35,51 @@ template <class Data, class Vertex> bool triangle()
   ALEPH_ASSERT_THROW( vr1.size()  == 7 );
 
   ALEPH_TEST_END();
-  return true;
+}
+
+template <class Data, class Vertex> void quad()
+{
+  ALEPH_TEST_BEGIN( "Quad" );
+
+  using Simplex           = Simplex<Data, Vertex>;
+  using SimplicialComplex = SimplicialComplex<Simplex>;
+
+  std::vector<Simplex> simplices
+    = { {0},
+        {1},
+        {2},
+        {3},
+        Simplex( {0,1}, Data(1) ),
+        Simplex( {1,2}, Data(1) ),
+        Simplex( {2,3}, Data(1) ),
+        Simplex( {0,3}, Data(1) ),
+        Simplex( {0,2}, Data( std::sqrt(2.0) ) ),
+        Simplex( {1,3}, Data( std::sqrt(2.0) ) ) };
+
+  SimplicialComplex K( simplices.begin(), simplices.end() );
+  RipsExpander<SimplicialComplex> ripsExpander;
+
+  auto vr1 = ripsExpander( K, 1 );
+  auto vr2 = ripsExpander( K, 2 );
+  auto vr3 = ripsExpander( K, 3 );
+
+  vr1 = ripsExpander.assignMaximumWeight( vr1 );
+  vr2 = ripsExpander.assignMaximumWeight( vr2 );
+  vr3 = ripsExpander.assignMaximumWeight( vr3 );
+
+  ALEPH_ASSERT_THROW( vr1.empty() == false );
+  ALEPH_ASSERT_THROW( vr2.empty() == false );
+  ALEPH_ASSERT_THROW( vr3.empty() == false );
+
+  ALEPH_ASSERT_THROW( vr1.size() == simplices.size()     );
+  ALEPH_ASSERT_THROW( vr2.size() == vr1.size()       + 4 ); // +4 triangles
+  ALEPH_ASSERT_THROW( vr3.size() == vr2.size()       + 1 ); // +1 tetrahedron
+
+  ALEPH_TEST_END();
 }
 
 int main()
 {
   triangle<double, unsigned>();
+  quad<double, unsigned>();
 }
