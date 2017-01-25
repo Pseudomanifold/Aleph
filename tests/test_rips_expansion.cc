8b7a525225a7b9f5314dfe627a3438adcf24de1d
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-01-25 11:08:06 +0100

    Added test case for filtration consistency
    As of now, I am expecting this code to fail because there is still a bug
    in the Rips expansion class.

--- tests/test_rips_expansion.cc
+++ tests/test_rips_expansion.cc
@@ -2,9 +2,13 @@
 
 #include "tests/Base.hh"
 
+#include "filtrations/Data.hh"
+
 #include "topology/Simplex.hh"
 #include "topology/SimplicialComplex.hh"
 
+#include <iterator>
+#include <set>
 #include <vector>
 
 #include <cmath>
@@ -12,6 +16,28 @@
 using namespace aleph::geometry;
 using namespace aleph::topology;
 using namespace aleph;
+
+/** Checks whether co-faces are preceded by their faces in a filtration */
+template <class InputIterator> bool isConsistentFiltration( InputIterator begin, InputIterator end )
+{
+  using Simplex = typename std::iterator_traits<InputIterator>::value_type;
+
+  // Contains all simplices that have already been 'seen' during the
+  // traversal. If an unseen face is encountered, we have identified
+  // an error.
+  std::set<Simplex> seen;
+
+  for( auto it = begin; it != end; ++it )
+  {
+    seen.insert( *it );
+
+    for( auto itFace = it->begin_boundary(); itFace != it->end_boundary(); ++itFace )
+      if( seen.find( *itFace ) == seen.end() )
+        return false;
+  }
+
+  return true;
+}
 
 template <class Data, class Vertex> void triangle()
 {
@@ -50,10 +76,10 @@ template <class Data, class Vertex> void quad()
         {2},
         {3},
         Simplex( {0,1}, Data(1) ),
+        Simplex( {0,2}, Data( std::sqrt(2.0) ) ),
         Simplex( {1,2}, Data(1) ),
         Simplex( {2,3}, Data(1) ),
         Simplex( {0,3}, Data(1) ),
-        Simplex( {0,2}, Data( std::sqrt(2.0) ) ),
         Simplex( {1,3}, Data( std::sqrt(2.0) ) ) };
 
   SimplicialComplex K( simplices.begin(), simplices.end() );
@@ -67,6 +93,10 @@ template <class Data, class Vertex> void quad()
   vr2 = ripsExpander.assignMaximumWeight( vr2 );
   vr3 = ripsExpander.assignMaximumWeight( vr3 );
 
+  vr1.sort( filtrations::Data<Simplex>() );
+  vr2.sort( filtrations::Data<Simplex>() );
+  vr3.sort( filtrations::Data<Simplex>() );
+
   ALEPH_ASSERT_THROW( vr1.empty() == false );
   ALEPH_ASSERT_THROW( vr2.empty() == false );
   ALEPH_ASSERT_THROW( vr3.empty() == false );
@@ -74,6 +104,10 @@ template <class Data, class Vertex> void quad()
   ALEPH_ASSERT_THROW( vr1.size() == simplices.size()     );
   ALEPH_ASSERT_THROW( vr2.size() == vr1.size()       + 4 ); // +4 triangles
   ALEPH_ASSERT_THROW( vr3.size() == vr2.size()       + 1 ); // +1 tetrahedron
+
+  ALEPH_ASSERT_THROW( isConsistentFiltration( vr1.begin(), vr1.end() ) );
+  ALEPH_ASSERT_THROW( isConsistentFiltration( vr2.begin(), vr2.end() ) );
+  ALEPH_ASSERT_THROW( isConsistentFiltration( vr3.begin(), vr3.end() ) );
 
   ALEPH_TEST_END();
 }
