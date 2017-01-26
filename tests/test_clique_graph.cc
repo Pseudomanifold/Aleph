db1953b86e8ede54f04d78027c0673a4b9f2cdaa
Bastian Rieck <bastian@rieck.ru>
2017-01-26 11:21:11 +0100

    Added simple clique graph test
    Just to ensure that I am not doing this completely wrong, let's add a
    test case for checking clique graph extraction.

---
+++ tests/test_clique_graph.cc
@@ -0,0 +1,34 @@
+#include "tests/Base.hh"
+
+#include "topology/CliqueGraph.hh"
+#include "topology/Simplex.hh"
+#include "topology/SimplicialComplex.hh"
+
+#include <vector>
+
+using namespace aleph::topology;
+using namespace aleph;
+
+template <class Data, class Vertex> void triangle()
+{
+  ALEPH_TEST_BEGIN( "Triangle" );
+
+  using Simplex           = Simplex<Data, Vertex>;
+  using SimplicialComplex = SimplicialComplex<Simplex>;
+
+  std::vector<Simplex> simplices
+    = { {0}, {1}, {2}, {0,1}, {0,2}, {1,2}, {0,1,2} };
+
+  SimplicialComplex K( simplices.begin(), simplices.end() );
+
+  auto C = getCliqueGraph( K, 1 );
+
+  ALEPH_ASSERT_THROW( C.empty() == false );
+
+  ALEPH_TEST_END();
+}
+
+int main()
+{
+  triangle<double, unsigned>();
+}
