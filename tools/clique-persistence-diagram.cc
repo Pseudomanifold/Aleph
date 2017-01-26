5cc42f432a27b8d7e6597afc4212decb9109aa48
Bastian Rieck <bastian@rieck.ru>
2017-01-26 15:18:40 +0100

    Started implementing clique persistence diagram implementation
    Something is still not working correctly with respect to the weights
    within the clique graph. Need to investigate.

---
+++ tools/clique-persistence-diagram.cc
@@ -0,0 +1,68 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "filtrations/Data.hh"
+
+#include "geometry/RipsExpander.hh"
+
+#include "persistentHomology/ConnectedComponents.hh"
+
+#include "topology/CliqueGraph.hh"
+#include "topology/Simplex.hh"
+#include "topology/SimplicialComplex.hh"
+
+#include "topology/io/EdgeLists.hh"
+
+using DataType           = double;
+using VertexType         = unsigned;
+using Simplex            = aleph::topology::Simplex<DataType, VertexType>;
+using SimplicialComplex  = aleph::topology::SimplicialComplex<Simplex>;
+using PersistenceDiagram = aleph::PersistenceDiagram<DataType>;
+
+int main( int argc, char** argv )
+{
+  if( argc <= 2 )
+  {
+    // TODO: Show usage
+    return -1;
+  }
+
+  std::string filename = argv[1];
+  unsigned k           = static_cast<unsigned>( std::stoul( argv[2] ) );
+
+  aleph::io::EdgeListReader reader;
+  reader.setReadWeights( true );
+  reader.setTrimLines( true );
+
+  SimplicialComplex K;
+
+  {
+    std::cerr << "* Reading '" << filename << "'...";
+
+    std::ifstream in( filename );
+    reader( in, K );
+
+    std::cerr << "finished\n";
+  }
+
+  aleph::geometry::RipsExpander<SimplicialComplex> ripsExpander;
+  K = ripsExpander( K, k );
+  K = ripsExpander.assignMaximumWeight( K );
+
+  K.sort( aleph::filtrations::Data<Simplex>() );
+
+  std::cerr << "* Extracting " << k << "-cliques graph...";
+
+  auto C
+      = aleph::topology::getCliqueGraph( K, k );
+
+  std::cerr << "finished\n";
+
+  std::cerr << "* " << k << "-cliques graph has " << C.size() << " simplices\n";
+
+  auto pd
+      = aleph::calculateZeroDimensionalPersistenceDiagram( K );
+
+  std::cout << pd << "\n";
+}
