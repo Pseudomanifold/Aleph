0088bafb190a245f934392d112a2940ec7d694e4
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-02-10 15:46:31 +0100

    Extended GML reader test
    Labels are being parsed correctly (albeit the reader ignores them at
    this stage). Basic parsing appears to work.

---
+++ tests/input/Simple_with_labels.gml
@@ -0,0 +1,31 @@
+comment Originally taken from https://gephi.org/users/supported-graph-formats/gml-format/
+graph
+[
+  node
+  [
+   id A
+   label "Node A"
+  ]
+  node
+  [
+   id B
+   label "Node B"
+  ]
+  node
+  [
+   id C
+   label "Node C"
+  ]
+   edge
+  [
+   source B
+   target A
+   label "Edge B to A"
+  ]
+  edge
+  [
+   source C
+   target A
+   label "Edge C to A"
+  ]
+]
--- tests/test_io_gml.cc
+++ tests/test_io_gml.cc
@@ -19,13 +19,18 @@ template <class D, class V> void test( const std::string& filename )
   aleph::topology::io::GMLReader reader;
   reader( filename, K );
 
+  ALEPH_ASSERT_EQUAL( K.size(), 5 );
+  ALEPH_ASSERT_EQUAL( std::count_if( K.begin(), K.end(), [] (const Simplex& s) { return s.dimension() == 0; } ), 3 );
+  ALEPH_ASSERT_EQUAL( std::count_if( K.begin(), K.end(), [] (const Simplex& s) { return s.dimension() == 1; } ), 2 );
+
   ALEPH_TEST_END();
 }
 
 int main()
 {
   std::vector<std::string> inputs = {
-    CMAKE_SOURCE_DIR + std::string( "/tests/input/Simple.gml" )
+    CMAKE_SOURCE_DIR + std::string( "/tests/input/Simple.gml" ),
+    CMAKE_SOURCE_DIR + std::string( "/tests/input/Simple_with_labels.gml" )
   };
 
   for( auto&& input : inputs )
