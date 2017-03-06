6a5e022a9b07b8f2398f85845ccbdcfc56b4a597
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-03-06 10:05:12 +0100

    Added Erdős--Rényi graph tests
    Only very simple ones so far but they indicate that the graph structure
    is not generated incorrectly.

---
+++ tests/test_graph_generation.cc
@@ -0,0 +1,40 @@
+#include "tests/Base.hh"
+#include "topology/RandomGraph.hh"
+
+#include <algorithm>
+
+using Simplex = aleph::topology::Simplex<short, unsigned>;
+
+auto isVertex = [] ( const Simplex& s )
+{
+  return s.dimension() == 0;
+};
+
+void testERG()
+{
+  ALEPH_TEST_BEGIN( "Erdos--Renyi graph" );
+
+  auto K0 = aleph::topology::generateErdosRenyiGraph( 10, 0.0 );
+  auto K1 = aleph::topology::generateErdosRenyiGraph( 10, 0.1 );
+  auto K2 = aleph::topology::generateErdosRenyiGraph( 10, 0.5 );
+  auto K3 = aleph::topology::generateErdosRenyiGraph( 10, 1.0 );
+
+  ALEPH_ASSERT_EQUAL( std::count_if( K0.begin(), K0.end(), isVertex ), 10 );
+  ALEPH_ASSERT_EQUAL( std::count_if( K1.begin(), K1.end(), isVertex ), 10 );
+  ALEPH_ASSERT_EQUAL( std::count_if( K2.begin(), K2.end(), isVertex ), 10 );
+  ALEPH_ASSERT_EQUAL( std::count_if( K3.begin(), K3.end(), isVertex ), 10 );
+
+  ALEPH_ASSERT_THROW( K0.size() <  K3.size() );
+  ALEPH_ASSERT_THROW( K1.size() <= K3.size() );
+  ALEPH_ASSERT_THROW( K2.size() <= K3.size() );
+
+  ALEPH_ASSERT_EQUAL( K0.size(), 10 );
+  ALEPH_ASSERT_EQUAL( K3.size(), 10 + 10*9 / 2 );
+
+  ALEPH_TEST_END();
+}
+
+int main( int, char** )
+{
+  testERG();
+}
