feb86e5ebfb529b6aba6f86f757d311b5023ba3b
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-03-06 18:16:07 +0100

    Removed obsolete comments

--- topology/io/GML.hh
+++ topology/io/GML.hh
@@ -324,30 +324,4 @@ private:
 
 } // namespace aleph
 
-// graph
-// [
-//   node
-//   [
-//    id A
-//   ]
-//   node
-//   [
-//    id B
-//   ]
-//   node
-//   [
-//    id C
-//   ]
-//    edge
-//   [
-//    source B
-//    target A
-//   ]
-//   edge
-//   [
-//    source C
-//    target A
-//   ]
-// ]
-
 #endif
