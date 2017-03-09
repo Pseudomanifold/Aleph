0d60755713eeea0eac5fed5daa6e381f1bcf88a5
Bastian Rieck <bastian@rieck.ru>
2017-03-09 13:56:48 +0100

    Extended test case for maximal clique enumeration
    The example is simple enough to be solved manually, so I am confident
    that the code is doing the right thing here. Of course, using a large
    graph would also be useful...

--- tests/test_clique_enumeration.cc
+++ tests/test_clique_enumeration.cc
@@ -60,6 +60,19 @@ template <class Data, class Vertex> void triangles()
   ALEPH_ASSERT_EQUAL( C11.size(), C12.size() );
   ALEPH_ASSERT_EQUAL( C21.size(), C22.size() );
 
+  ALEPH_ASSERT_EQUAL( C11.size(), 2 );
+  ALEPH_ASSERT_EQUAL( C21.size(), 3 );
+
+  ALEPH_ASSERT_THROW( C11 == C12 );
+  ALEPH_ASSERT_THROW( C21 == C22 );
+
+  ALEPH_ASSERT_THROW( std::find( C11.begin(), C11.end(), std::set<Vertex>( {0,1,2} ) ) != C11.end() );
+  ALEPH_ASSERT_THROW( std::find( C11.begin(), C11.end(), std::set<Vertex>( {0,1,3} ) ) != C11.end() );
+
+  ALEPH_ASSERT_THROW( std::find( C21.begin(), C21.end(), std::set<Vertex>( {0,3  } ) ) != C21.end() );
+  ALEPH_ASSERT_THROW( std::find( C21.begin(), C21.end(), std::set<Vertex>( {0,1,2} ) ) != C21.end() );
+  ALEPH_ASSERT_THROW( std::find( C21.begin(), C21.end(), std::set<Vertex>( {3,4,5} ) ) != C21.end() );
+
   ALEPH_TEST_END();
 }
 
