3d227db54877412cb7a0056a30f59040feac5f88
Bastian Rieck <bastian@rieck.ru>
2017-03-09 09:27:35 +0100

    Bron--Kerbosch clique enumeration (preliminary & untested)
    Decided to implement this algorithm prior to attempting the
    implementation of Koch's variant. This algorithm requires a
    conversion of the simplicial complex to an adjacency matrix
    representing connectivity in the $1$-skeleton.
    
    Potential issues:
    * Conversion performance
    * Not exploiting symmetry in adjacency relations
    * Vertex lookup uses zero-based indices

--- topology/MaximalCliques.hh
+++ topology/MaximalCliques.hh
@@ -1,6 +1,7 @@
 #ifndef ALEPH_TOPOLOGY_MAXIMAL_CLIQUES_HH__
 #define ALEPH_TOPOLOGY_MAXIMAL_CLIQUES_HH__
 
+#include <algorithm>
 #include <iterator>
 #include <set>
 #include <unordered_set>
@@ -76,6 +77,55 @@ void enumerateKoch( std::unordered_set<typename Simplex::VertexType>& C,
    */
 }
 
+template <class Simplex>
+void enumerateBronKerbosch( std::unordered_set<typename Simplex::VertexType>& C,
+                            std::unordered_set<typename Simplex::VertexType>& I,
+                            std::unordered_set<typename Simplex::VertexType>& X,
+                            std::vector< std::vector<typename Simplex::VertexType> >& cliques,
+                            const math::SparseBinaryMatrix<typename Simplex::VertexType>& A )
+{
+  using VertexType = typename Simplex::VertexType;
+
+  if( I.empty() && X.empty() )
+  {
+    cliques.push_back( std::vector<VertexType>( C.begin(), C.end() ) );
+    return;
+  }
+
+  for( auto it = I.begin(); it != I.end(); ++it )
+  {
+    auto element = *it;
+    it = I.erase( it );
+
+    auto newC = C;
+    newC.insert( element );
+
+    std::unordered_set<VertexType> neighbours;
+    A.get( element, std::inserter( neighbours, neighbours.begin() );
+
+    std::unordered_set<VertexType> newI;
+    std::unordered_set<VertexType> newX;
+
+    std::set_intersection( I.begin(), I.end(),
+                           neighbours.begin(), neighbours.end(),
+                           std::inserter( newI,
+                                          newI.begin() ) );
+
+    std::set_intersection( X.begin(), X.end(),
+                           neighbours.begin(), neighbours.end(),
+                           std::inserter( newX,
+                                          newX.begin() ) );
+
+    enumerateBronKerbosch( newC,
+                           newI,
+                           newX,
+                           cliques,
+                           A );
+
+    X.insert( element );
+  }
+}
+
 } // namespace detail
 
 /**
@@ -100,6 +150,22 @@ template <class Simplex> auto maximalCliquesKoch( const SimplicialComplex<Simple
   std::vector< std::vector<VertexType> > cliques;
 
   detail::enumerateKoch( C, I, X, cliques, K );
+  return cliques;
+}
+
+template <class Simplex> auto maximalCliquesBronKerbosch( const SimplicialComplex<Simplex>& K ) -> std::vector< std::vector<typename Simplex::VertexType> >
+{
+  using VertexType = typename Simplex::VertexType;
+
+  std::unordered_set<VertexType> C;
+  std::unordered_set<VertexType> I;
+  std::unordered_set<VertexType> X;
+
+  K.vertices( std::inserter( I, I.begin() ) );
+
+  std::vector< std::vector<VertexType> > cliques;
+
+  detail::enumerateBronKerbosch( C, I, X, cliques, detail::adjacencyMatrix(K) );
   return cliques;
 }
 
