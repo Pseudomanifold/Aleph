fbdf9a15d1a00f42c439d7426278b7999c0de04a
Bastian Rieck <bastian@rieck.ru>
2017-03-12 17:16:16 +0100

    Generating all combinations of maximal simplices
    This follows the suggestion by Zomorodian for generating the simplicial
    complex in a top-down manner.
    
    I have not yet fully tested the code, though. In particular, it will be
    interesting to see whether expansion also works if a minimum and maximum
    value for $k$ are being used.

--- geometry/RipsExpanderTopDown.hh
+++ geometry/RipsExpanderTopDown.hh
@@ -1,6 +1,7 @@
 #ifndef ALEPH_GEOMETRY_RIPS_EXPANDER_TOP_DOWN_HH__
 #define ALEPH_GEOMETRY_RIPS_EXPANDER_TOP_DOWN_HH__
 
+#include <algorithm>
 #include <list>
 #include <vector>
 
@@ -12,6 +13,68 @@ namespace aleph
 namespace geometry
 {
 
+namespace detail
+{
+
+/*
+  Originally written by Thomas Draper for an article of Mark Nelson in the
+  C/C++ users journal. See
+
+    http://marknelson.us/2002/03/01/next-permutation/
+
+  for more information.
+*/
+
+template <class Iterator> bool next_combination( const Iterator first, Iterator k, const Iterator last )
+{
+  if( first == last || first == k || last == k )
+    return false;
+
+  auto itr1 = first;
+  auto itr2 = last;
+  ++itr1;
+
+  if(last == itr1)
+    return false;
+
+  itr1 = last;
+  --itr1;
+  itr1 = k;
+  --itr2;
+
+  while( first != itr1 )
+  {
+    if( *--itr1 < *itr2 )
+    {
+      auto j = k;
+      while( !(*itr1 < *j) )
+        ++j;
+
+      std::iter_swap( itr1, j );
+
+      ++itr1;
+      ++j;
+      itr2 = k;
+
+      std::rotate( itr1, j, last );
+
+      while( last != j )
+      {
+        ++j;
+        ++itr2;
+      }
+
+      std::rotate( k, itr2, last );
+      return true;
+    }
+  }
+
+  std::rotate( first, k, last );
+  return false;
+}
+
+} // namespace detail
+
 template <class SimplicialComplex> class RipsExpanderTopDown
 {
 public:
@@ -21,20 +84,29 @@ public:
 
   SimplicialComplex operator()( const SimplicialComplex& K, unsigned kMax )
   {
-    return this->operator()( K, kMax, 0 );
+    return this->operator()( K, kMax, 1 );
   }
 
   SimplicialComplex operator()( const SimplicialComplex& K, unsigned kMax, unsigned kMin )
   {
-    (void) kMax;
-    (void) kMin;
-
     auto maximalCliques = aleph::topology::maximalCliquesKoch( K );
 
     std::list<Simplex> simplices;
 
     for( auto&& clique : maximalCliques )
-      simplices.push_back( Simplex( clique.begin(), clique.end() ) );
+    {
+      auto C = std::vector<VertexType>( clique.begin(), clique.end() );
+
+      for( unsigned k = kMin; k <= std::min( kMax, unsigned( C.size() ) ); k++ )
+      {
+        do
+        {
+          simplices.push_back( Simplex( C.begin(), C.begin() + k ) );
+        }
+        while( detail::next_combination( C.begin(), C.begin() + k, C.end() ) );
+      }
+
+    }
 
     return SimplicialComplex( simplices.begin(), simplices.end() );
   }
