1831505015acd1ec9fa4775807cfbdfb2e5ea450
Bastian Rieck <bastian@rieck.ru>
2017-03-14 09:49:56 +0100

    Implemented distance calculations between matching indicator functions
    Something is still wrong here, though, as the calculation crashes at the
    moment. I hope that this is related to an empty indicator function---and
    not to something inherently wrong with their calculation.

--- tools/persistence-indicator-function-distance.cc
+++ tools/persistence-indicator-function-distance.cc
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <limits>
 #include <map>
 #include <regex>
 #include <string>
@@ -28,6 +30,39 @@ struct DataSet
   PersistenceIndicatorFunction persistenceIndicatorFunction;
 };
 
+// Calculates the distance between two data sets. This requires enumerating all
+// dimensions, and finding the corresponding persistence indicator function. If
+// no such function exists, the function uses the norm of the function.
+double distance( const std::vector<DataSet>& dataSet1, const std::vector<DataSet>& dataSet2, unsigned minDimension, unsigned maxDimension )
+{
+  auto getPersistenceIndicatorFunction = [] ( const std::vector<DataSet>& dataSet, unsigned dimension )
+  {
+    auto it = std::find_if( dataSet.begin(), dataSet.end(),
+                            [&dimension] ( const DataSet& dataSet )
+                            {
+                              return dataSet.dimension == dimension;
+                            } );
+
+    if( it != dataSet.end() )
+      return it->persistenceIndicatorFunction;
+    else
+      return PersistenceIndicatorFunction();
+  };
+
+  double d = 0.0;
+
+  for( unsigned dimension = minDimension; dimension <= maxDimension; dimension++ )
+  {
+    auto f = getPersistenceIndicatorFunction( dataSet1, dimension );
+    auto g = getPersistenceIndicatorFunction( dataSet2, dimension );
+
+    g = g * (-1.0);
+    d = d + (f+g).integral();
+  }
+
+  return d;
+}
+
 int main( int argc, char** argv )
 {
   if( argc <= 1 )
@@ -48,6 +83,9 @@ int main( int argc, char** argv )
 
   // Get filenames & prefixes ------------------------------------------
 
+  unsigned minDimension = std::numeric_limits<unsigned>::max();
+  unsigned maxDimension = 0;
+
   {
     std::vector<std::string> filenames;
     filenames.reserve( argc - 1 );
@@ -63,11 +101,14 @@ int main( int argc, char** argv )
         auto dimension = unsigned( std::stoul( matches[2] ) );
 
         dataSets[name].push_back( { filename, dimension, {}, {} } );
+
+        minDimension = std::min( minDimension, dimension );
+        maxDimension = std::max( maxDimension, dimension );
       }
     }
   }
 
-  // Load persistence diagrams -----------------------------------------
+  // Load persistence diagrams & calculate indicator functions ---------
 
   std::vector<PersistenceDiagram> persistenceDiagrams;
 
@@ -89,6 +130,27 @@ int main( int argc, char** argv )
          = aleph::persistenceIndicatorFunction( dataSet.persistenceDiagram );
 
       std::cerr << "finished\n";
+    }
+  }
+
+  // Calculate all distances -------------------------------------------
+
+  std::vector< std::vector<double> > distances;
+  distances.resize( dataSets.size(), std::vector<double>( dataSets.size() ) );
+
+  std::size_t row = 0;
+
+  for( auto it1 = dataSets.begin(); it1 != dataSets.end(); ++it1, ++row )
+  {
+    std::size_t col = 0;
+    for( auto it2 = std::next( it1 ); it2 != dataSets.end(); ++it2, ++col )
+    {
+      auto d = distance( it1->second, it2->second, minDimension, maxDimension );
+
+      distances[row][col] = d;
+      distances[col][row] = d;
+
+      std::cerr << "D[" << row << "][" << col << "]: " << d << "\n";
     }
   }
 }
