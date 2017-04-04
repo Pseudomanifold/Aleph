99f022c14946c05f0616e31e232f561139f4285b
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-04-04 18:42:24 +0200

    Added test case for meshes
    This is absolutely useless at this point because nothing is really
    tested. But at least I know that the mesh construction does not do
    anything stupid.
    
    Moreover, I can use this test case to test other algorithms on the
    mesh data structure.

---
+++ tests/test_mesh.cc
@@ -0,0 +1,52 @@
+#include "tests/Base.hh"
+
+#include "topology/Mesh.hh"
+
+#include <vector>
+
+void test1()
+{
+  aleph::topology::Mesh<double> M;
+
+  M.addVertex( 0.0, 0.0, 0.0 );
+  M.addVertex( 0.0, 1.0, 0.0 );
+  M.addVertex( 1.0, 0.0, 0.0 );
+  M.addVertex( 1.5, 1.0, 0.0 );
+
+  std::vector<unsigned> f1 = { 0, 1, 2 };
+  std::vector<unsigned> f2 = { 2, 1, 3 };
+
+  M.addFace( f1.begin(), f1.end() );
+  M.addFace( f2.begin(), f2.end() );
+}
+
+void test2()
+{
+  aleph::topology::Mesh<double> M;
+
+  M.addVertex( 0.0, 0.0, 0.0, 0.0 );
+  M.addVertex( 1.0, 0.0, 0.0, 1.0 );
+  M.addVertex( 2.0, 0.0, 0.0, 0.0 );
+  M.addVertex( 0.0, 1.0, 0.0, 1.0 );
+  M.addVertex( 1.0, 1.0, 0.0, 2.0 );
+  M.addVertex( 2.0, 1.0, 0.0, 1.0 );
+  M.addVertex( 0.0, 2.0, 0.0, 0.0 );
+  M.addVertex( 1.0, 2.0, 0.0, 1.0 );
+  M.addVertex( 2.0, 2.0, 0.0, 0.0 );
+
+  std::vector<unsigned> f1 = { 0, 1, 4, 3 };
+  std::vector<unsigned> f2 = { 1, 2, 5, 4 };
+  std::vector<unsigned> f3 = { 4, 5, 8, 7 };
+  std::vector<unsigned> f4 = { 3, 4, 7, 6 };
+
+  M.addFace( f1.begin(), f1.end() );
+  M.addFace( f2.begin(), f2.end() );
+  M.addFace( f3.begin(), f3.end() );
+  M.addFace( f4.begin(), f4.end() );
+}
+
+int main(int, char**)
+{
+  test1();
+  test2();
+}
