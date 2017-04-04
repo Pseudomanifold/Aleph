cfb66902b8d34055fcedba1d60c5786b441987de
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-04-04 19:12:48 +0200

    Basic vertex classification for Morse--Smale complexes
    Based on the lower/higher neighbours, I can now classify vertices as
    either minimum, saddle, or maximum.

--- topology/MorseSmaleComplex.hh
+++ topology/MorseSmaleComplex.hh
@@ -1,6 +1,9 @@
 #ifndef ALEPH_TOPOLOGY_MORSE_SMALE_COMPLEX__
 #define ALEPH_TOPOLOGY_MORSE_SMALE_COMPLEX__
 
+// TODO: Remove after debugging
+#include <iostream>
+
 namespace aleph
 {
 
@@ -16,6 +19,22 @@ public:
 
     for( decltype(n) i = 0; i < n; i++ )
     {
+      auto v = M.vertex(i);
+
+      auto higherNeigbours = M.getHigherNeighbours( *v );
+      auto lowerNeighbours = M.getLowerNeighbours( *v );
+
+      std::cerr << "No. higher neighbours: " << higherNeigbours.size() << "\n"
+                << "No. lower  neighbours: " << lowerNeighbours.size() << "\n";
+
+      std::cerr << "[" << i << "]: ";
+
+      if( higherNeigbours.empty() )
+        std::cerr << "Maximum\n";
+      else if( lowerNeighbours.empty() )
+        std::cerr << "Minimum\n";
+      else
+        std::cerr << "Saddle\n";
     }
   }
 };
