c1a8e21abb3afa668224bd9bcca581504c026197
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-04-10 16:51:11 +0200

    Added (incorrect) calculation of contiguous segments for Morse--Smale complex
    This is just for testing whether the connectivity calculation test for
    the upper and lower link of a vertex work in principle.

--- topology/MorseSmaleComplex.hh
+++ topology/MorseSmaleComplex.hh
@@ -4,6 +4,8 @@
 // TODO: Remove after debugging
 #include <iostream>
 
+#include "topology/UnionFind.hh"
+
 namespace aleph
 {
 
@@ -22,18 +24,72 @@ public:
       auto higherNeigbours = M.getHigherNeighbours( vertex );
       auto lowerNeighbours = M.getLowerNeighbours( vertex );
 
-      std::cerr << "No. higher neighbours: " << higherNeigbours.size() << "\n"
-                << "No. lower  neighbours: " << lowerNeighbours.size() << "\n";
-
       std::cerr << "[" << vertex << "]: ";
 
       if( higherNeigbours.empty() )
-        std::cerr << "Maximum\n";
+      {
+        std::cerr << "Maximum\n"
+                  << "  k = " << contiguousSegments( M, vertex ) << "\n";
+      }
       else if( lowerNeighbours.empty() )
-        std::cerr << "Minimum\n";
+        std::cerr << "Minimum\n"
+                  << "  k = " << contiguousSegments( M, vertex ) << "\n";
       else
-        std::cerr << "Saddle\n";
+      {
+        std::cerr << "Saddle\n"
+                  << "  k = " << contiguousSegments( M, vertex ) << "\n";
+
+        std::cerr << "  + ";
+        for( auto&& neighbour : higherNeigbours )
+          std::cerr << neighbour << " ";
+        std::cerr << "\n";
+
+        std::cerr << "  - ";
+        for( auto&& neighbour : lowerNeighbours )
+          std::cerr << neighbour << " ";
+        std::cerr << "\n";
+      }
     }
+  }
+private:
+
+  /**
+    Calculates the number of contiguous segments in the link of
+    a vertex, with respect to the given mesh.
+  */
+
+  static std::size_t contiguousSegments( const Mesh& M, typename Mesh::Index id )
+  {
+    using Index = typename Mesh::Index;
+
+    auto link = M.link(id);
+    auto curr = link.begin();
+    auto next = std::next( curr );
+
+    UnionFind<Index> uf( link.begin(), link.end() );
+
+    for( ; curr != link.end(); )
+    {
+      if( next == link.end() )
+        next = link.begin();
+
+      auto u    = id;
+      auto v    = *curr++;
+      auto w    = *next++;
+
+      if( M.hasEdge(u,v) )
+      {
+        if( M.hasEdge(v,w) )
+          uf.merge(v,w);
+      }
+      else
+        throw std::runtime_error( "Edge to link centre vertex does not exist" );
+    }
+
+    std::vector<Index> roots;
+    uf.roots( std::back_inserter(roots) );
+
+    return roots.size();
   }
 };
 
