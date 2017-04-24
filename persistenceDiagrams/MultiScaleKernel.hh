3875be58b491297520db4f79fd36d7ab1025c6c3
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-04-24 11:15:34 +0200

    Implementation of multi-scale kernel for persistence diagrams
    Using an older implementation of mine so far that I just resurrected. It
    should be compliant with the interface of Aleph, but I do not yet have a
    meaningful test suite for it.

---
+++ persistenceDiagrams/MultiScaleKernel.hh
@@ -0,0 +1,100 @@
+#ifndef ALEPH_MULTI_SCALE_KERNEL_HH__
+#define ALEPH_MULTI_SCALE_KERNEL_HH__
+
+#include "persistenceDiagrams/PersistenceDiagram.hh"
+
+#include <algorithm>
+#include <utility>
+
+#include <cmath>
+
+namespace aleph
+{
+
+namespace detail
+{
+
+/**
+  Calculates the squared Euclidean distance between points in
+  a persistence diagram. If specified, the second point, $q$,
+  is mirrored at the diagonal.
+*/
+
+template <class Point> double squaredEuclideanDistance( const Point& p,
+                                                        const Point& q,
+                                                        bool mirror = false )
+{
+  auto x0 = p.x();
+  auto y0 = p.y();
+  auto x1 = q.x();
+  auto x2 = q.y();
+
+  if( mirror )
+    std::swap( x1, y1 );
+
+  // The check ensures that dx and dy are always positive. Thus, even if
+  // the underlying data type is an unsigned type, both expressions will
+  // evaluate properly.
+  auto dx = x0 > x1 ? x0-x1 : x1-x0;
+  auto dy = y0 > y1 ? y0-y1 : y1-y0;
+
+  return static_cast<double>( dx*dx + dy*dy );
+}
+
+} // namespace detail
+
+template <class T> double multiScaleKernel( const PersistenceDiagram<T>& D1,
+                                            const PersistenceDiagram<T>& D2,
+                                            double sigma )
+{
+  SKahanSummation<double> sum = 0.0;
+
+  for( auto&& p : D1 )
+  {
+    for( auto&& q : D2 )
+    {
+      auto d1 = detail::squaredEuclideanDistance( p, q );
+      auto d2 = detail::squaredEuclideanDistance( p, q, true );
+
+      sum += std::exp( -d1 / ( 8.0*M_PI ) );
+      sum -= std::exp( -d2 / ( 8.0*M_PI ) );
+    }
+  }
+
+  return 1.0 / ( 8.0*M_PI*sigma ) * sum;
+}
+
+template <class T> double multiScaleFeatureMap( const PersistenceDiagram<T>& D,
+                                                double sigma )
+{
+  SKahanSummation<double> result = 0.0;
+
+  for( auto&& p : pairing )
+  {
+    for( auto&& q : pairing )
+    {
+      auto d1 = detail::squaredEuclideanDistance( p, q );
+      auto d2 = detail::squaredEuclideanDistance( p, q, true );
+
+      result += std::exp( -d1 / (4.0*M_PI) );
+      result -= std::exp( -d2 / (4.0*M_PI) );
+    }
+  }
+
+  return 1.0 / ( 4.0*M_PI*sigma ) * result;
+}
+
+template <class T> double multiScalePseudoMetric( const PersistenceDiagram<T>& D1,
+                                                  const PersistenceDiagram<T>& D2,
+                                                  double sigma )
+{
+  auto kxx = multiScaleKernel( D1, D1, sigma );
+  auto kxy = multiScaleKernel( D1, D2, sigma );
+  auto kyy = multiScaleKernel( D2, D2, sigma );
+
+  return std::sqrt( kxx + kyy - 2*kxy );
+}
+
+}
+
+#endif
