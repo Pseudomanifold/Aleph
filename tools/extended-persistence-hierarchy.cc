025e815a08dd73629c14e0ed877884b74b9f8bd1
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-05-24 13:43:47 +0200

    Made filtration type configurable
    This is not the most elegant way of solving this but at least it is now
    possible to configure the persistence hierarchy calculation.

--- tools/extended-persistence-hierarchy.cc
+++ tools/extended-persistence-hierarchy.cc
@@ -13,6 +13,7 @@
 
 #include "utilities/Filesystem.hh"
 
+#include <functional>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -30,24 +31,56 @@ using PersistenceDiagram = aleph::PersistenceDiagram<DataType>;
 void usage()
 {
   // TODO: Not yet implemented
+  std::cerr << "Usage: extended-persistence-hierarchy [--superlevels] [--sublevels] FILES\n";
 }
 
 int main( int argc, char** argv )
 {
-  if( argc <= 1 )
+  static option commandLineOptions[] =
+  {
+    { "superlevels", no_argument, nullptr, 'S' },
+    { "sublevels"  , no_argument, nullptr, 's' },
+    { nullptr      , 0          , nullptr,  0  }
+  };
+
+  bool calculateSuperlevelSets = false;
+
+  int option = 0;
+  while( ( option = getopt_long( argc, argv, "Ss", commandLineOptions, nullptr ) ) != -1 )
+  {
+    switch( option )
+    {
+    case 'S':
+      calculateSuperlevelSets = true;
+      break;
+    case 's':
+      calculateSuperlevelSets = false;
+      break;
+    default:
+      break;
+    }
+  }
+
+  if( ( argc - optind ) <= 0 )
   {
     usage();
     return -1;
   }
 
   std::vector<std::string> filenames;
-  filenames.reserve( argc - 1 );
+  filenames.reserve( argc - optind );
 
-  for( int i = 1; i < argc; i++ )
+  for( int i = optind; i < argc; i++ )
     filenames.push_back( argv[i] );
 
   std::vector<SimplicialComplex> simplicialComplexes;
   simplicialComplexes.reserve( filenames.size() );
+
+  // Select a functor for calculating the weights when reading
+  // a simplicial complex.
+  auto functor = calculateSuperlevelSets
+               ? [] ( DataType a, DataType b ) { return std::min(a,b); }
+               : [] ( DataType a, DataType b ) { return std::max(a,b); };
 
   for( auto&& filename : filenames )
   {
@@ -58,18 +91,28 @@ int main( int argc, char** argv )
       SimplicialComplex K;
 
       aleph::topology::io::VTKStructuredGridReader reader;
-      reader( filename, K );
+      reader( filename, K, functor );
 
-      K.sort( aleph::filtrations::Data<Simplex>() );
+      if( calculateSuperlevelSets )
+        K.sort( aleph::filtrations::Data<Simplex, std::greater<DataType> >() );
+      else
+        K.sort( aleph::filtrations::Data<Simplex, std::less<DataType> >() );
 
       simplicialComplexes.emplace_back( K );
     }
     else
     {
-      auto complexes = aleph::topology::io::loadFunctions<SimplicialComplex>( filename );
+      auto complexes
+        = aleph::topology::io::loadFunctions<SimplicialComplex>( filename,
+                                                                 functor );
 
       for( auto&& K : complexes )
-        K.sort( aleph::filtrations::Data<Simplex>() );
+      {
+        if( calculateSuperlevelSets )
+          K.sort( aleph::filtrations::Data<Simplex, std::less<DataType> >() );
+        else
+          K.sort( aleph::filtrations::Data<Simplex, std::greater<DataType> >() );
+      }
 
       simplicialComplexes.insert( simplicialComplexes.end(),
                                   complexes.begin(), complexes.end() );
