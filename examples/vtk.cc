b5242b1adfedae0f197ae9118fe5a6b5530a4f60
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-05-29 18:44:18 +0200

    Created stub for VTK file processing example
    Will continue here tomorrow---I need such a tool anyway in order to
    obtain persistence diagrams for the climate data sets.

---
+++ examples/vtk.cc
@@ -0,0 +1,26 @@
+/*
+  This is an example file shipped by  'Aleph - A Library for Exporing
+  Persistent Homology'.
+
+  This example demonstrates how to load a VTK structured grid from
+  a file, convert it into a simplicial complex, and calculate its
+  persistent homology.
+
+  Demonstrated classes:
+
+    - aleph::PersistenceDiagram
+    - aleph::topology::Simplex
+    - aleph::topology::SimplicialComplex
+    - aleph::topology::VTKStructuredGridReader
+
+  Demonstrated functions:
+
+    - aleph::calculatePersistenceDiagrams
+    - Simplicial complex sorting (for filtrations)
+
+  Original author: Bastian Rieck
+*/
+
+int main(int, char**)
+{
+}
