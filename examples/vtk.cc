49b13a67a2a4f92c5cd1a33cfe4a9ca3a1dbb6b6
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-05-30 08:21:15 +0200

    Added usage information & basic command-line option handling

--- examples/vtk.cc
+++ examples/vtk.cc
@@ -16,11 +16,61 @@
   Demonstrated functions:
 
     - aleph::calculatePersistenceDiagrams
+    - aleph::utilities::extension
     - Simplicial complex sorting (for filtrations)
 
   Original author: Bastian Rieck
 */
 
-int main(int, char**)
+#include <getopt.h>
+
+#include <iostream>
+
+void usage()
 {
+  std::cerr << "Usage: vtk [--superlevels] [--sublevels] FILE\n"
+            << "\n"
+            << "Calculates persistent homology of an input file. This program only\n"
+            << "handles VTK files. The output is a persistence diagram and will be\n"
+            << "written to STDOUT.\n\n"
+            << "Flags:\n"
+            << "  -S --superlevels: calculate superlevel sets\n"
+            << "  -s --sublevels  : calculate sublevel sets\n"
+            << "\n";
+}
+
+int main( int argc, char** argv )
+{
+  static option commandLineOptions[] =
+  {
+    { "superlevels", no_argument, nullptr, 'S' },
+    { "sublevels"  , no_argument, nullptr, 's' },
+    { nullptr      , 0          , nullptr,  0  }
+  };
+
+  bool calculateSuperlevelSets = false;
+
+  int option = 0;
+  while( ( option = getopt_long( argc, argv, "Ss", commandLineOptions, nullptr ) ) != -1 )
+  {
+    switch( option )
+    {
+    case 'S':
+      calculateSuperlevelSets = true;
+      break;
+    case 's':
+      calculateSuperlevelSets = false;
+      break;
+    default:
+      break;
+    }
+  }
+
+  if( ( argc - optind ) <= 0 )
+  {
+    usage();
+    return -1;
+  }
+
+
 }
