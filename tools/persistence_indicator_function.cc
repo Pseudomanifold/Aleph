523aa050fc8dafde280ba505c075518bf8ef9d35
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-06-14 10:59:00 +0200

    Implemented mean persistence indicator function calculation

--- tools/persistence_indicator_function.cc
+++ tools/persistence_indicator_function.cc
@@ -1,6 +1,19 @@
+/*
+  This is a tool shipped by 'Aleph - A Library for Exploring Persistent
+  Homology'.
+
+  Given a set of persistence diagrams, it calculates their persistence
+  indicator functions and---optionally---their mean indicator function
+  if specified by the client.
+
+  All files will be written to '/tmp', prefixed with 'PIF_'.
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
+
+#include <getopt.h>
 
 #include "persistenceDiagrams/PersistenceDiagram.hh"
 #include "persistenceDiagrams/PersistenceIndicatorFunction.hh"
@@ -14,18 +27,41 @@ using PersistenceDiagram = aleph::PersistenceDiagram<DataType>;
 
 int main( int argc, char** argv )
 {
-  if( argc <= 1 )
+  static option commandLineOptions[] =
+  {
+    { "mean" , no_argument, nullptr, 'm' },
+    { nullptr, 0          , nullptr,  0  }
+  };
+
+  if( ( argc - optind ) < 1 )
   {
     // TODO: Show usage
     return -1;
   }
 
+  bool calculateMean = false;
+
+  {
+    int option = 0;
+    while( ( option = getopt_long( argc, argv, "m", commandLineOptions, nullptr ) ) != -1 )
+    {
+      switch( option )
+      {
+      case 'm':
+        calculateMean = true;
+        break;
+      default:
+        break;
+      }
+    }
+  }
+
   // Get filenames -----------------------------------------------------
 
   std::vector<std::string> filenames;
-  filenames.reserve( argc - 1 );
+  filenames.reserve( argc - optind );
 
-  for( int i = 1; i < argc; i++ )
+  for( int i = optind; i < argc; i++ )
     filenames.push_back( argv[i] );
 
   // Load persistence diagrams -----------------------------------------
@@ -52,11 +88,16 @@ int main( int argc, char** argv )
 
   // Calculate persistence indicator functions -------------------------
 
+  decltype( aleph::persistenceIndicatorFunction( PersistenceDiagram() ) ) mean;
+
   unsigned i = 0;
   for( auto&& D : persistenceDiagrams )
   {
     auto f        = aleph::persistenceIndicatorFunction( D );
     auto filename = filenames.at(i);
+
+    if( calculateMean )
+      mean += f;
 
     using namespace aleph::utilities;
 
@@ -68,5 +109,16 @@ int main( int argc, char** argv )
     out << f << "\n";
 
     ++i;
+  }
+
+  if( calculateMean )
+  {
+    mean                /= static_cast<DataType>( persistenceDiagrams.size() );
+    auto outputFilename  = "/tmp/PIF_mean.txt";
+
+    std::cerr << "* Writing mean persistence indicator function to '" << outputFilename << "'...\n";
+
+    std::ofstream out( outputFilename );
+    out << mean << "\n";
   }
 }
