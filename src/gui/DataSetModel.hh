121ed772b2ffdf3bcf3f198829ef5c60a4220fd8
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-11 14:24:49 +0200

    Removed obsolete test code

--- src/gui/DataSetModel.hh
+++ src/gui/DataSetModel.hh
@@ -4,32 +4,6 @@
 #include <QAbstractItemModel>
 #include <QObject>
 
-#if 0
-class TreeModel : public QAbstractItemModel
-{
-    Q_OBJECT
-
-public:
-    explicit TreeModel(const QString &data, QObject *parent = 0);
-    ~TreeModel();
-
-    QVariant data(const QModelIndex &index, int role) const override;
-    Qt::ItemFlags flags(const QModelIndex &index) const override;
-    QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const override;
-    QModelIndex index(int row, int column,
-                      const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &index) const override;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-
-private:
-    void setupModelData(const QStringList &lines, TreeItem *parent);
-
-    TreeItem *rootItem;
-};
-#endif
-
 namespace aleph
 {
 
