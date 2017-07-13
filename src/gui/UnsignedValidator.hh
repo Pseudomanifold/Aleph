c6543b70764a987bfa943ba98e9c9c29ed7c4404
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-13 09:37:30 +0200

    Added validator for unsigned values

---
+++ src/gui/UnsignedValidator.hh
@@ -0,0 +1,52 @@
+#ifndef ALEPH_GUI_UNSIGNED_VALIDATOR_HH__
+#define ALEPH_GUI_UNSIGNED_VALIDATOR_HH__
+
+#include <QValidator>
+
+namespace aleph
+{
+
+namespace gui
+{
+
+class UnsignedValidator : public QValidator
+{
+  Q_OBJECT
+
+public:
+
+  /** Creates a new validator object */
+  UnsignedValidator( QObject* parent = 0 );
+
+  virtual ~UnsignedValidator()
+  {
+  }
+
+  /**
+    Checks whether the input string is valid. If this is not the case, the
+    string will be replaced by the string containing a zero, i.e. "0".
+
+    @param string Input string (will be changed)
+  */
+
+  virtual void fixup( QString& string ) const;
+
+  /**
+    Validates user input. To this end, the function checks whether the
+    input string is a valid unsigned value.
+
+    @param input Input string (will not be changed)
+    @param pos   Position (unused)
+
+    @returns QValidator::Acceptable or QValidator::Invalid, depending on
+    the type check.
+  */
+
+  virtual State validate( QString& input , int& pos ) const;
+};
+
+} // namespace gui
+
+} // namespace aleph
+
+#endif
