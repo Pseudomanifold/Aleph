c0a5b96d6c6a07ae9d40266e2f0524d22d8ea37a
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-25 15:18:33 +0200

    Model for intersection homology perversities

--- include/aleph/persistentHomology/PhiPersistence.hh
+++ include/aleph/persistentHomology/PhiPersistence.hh
@@ -1,11 +1,11 @@
 #ifndef ALEPH_PERSISTENT_HOMOLOGY_PHI_PERSISTENCE_HH__
 #define ALEPH_PERSISTENT_HOMOLOGY_PHI_PERSISTENCE_HH__
 
-
-#include <aleph/topology/Conversions.hh>
 #include <aleph/topology/SimplicialComplex.hh>
 
+#include <initializer_list>
 #include <utility>
+#include <vector>
 
 namespace aleph
 {
@@ -45,6 +45,48 @@ template <class Simplex, class Function> std::pair<topology::SimplicialComplex<S
 
   return std::make_pair( L, s );
 }
+
+/**
+  Models a perversity in the sense of intersection homology. The class
+  ensures that all values satisfy
+
+  \f
+    -1 \leq p_k \leq k-1
+  \f
+*/
+
+class Perversity
+{
+public:
+  Perversity( std::initializer_list<int> values )
+  {
+    _values.assign( values.begin(), values.end() );
+
+    for( std::size_t k = 0; k < _values.size(); k++ )
+    {
+      if( _values[k] < -1 )
+        _values[k] = -1;
+      else if( _values[k] >= static_cast<int>( k-1 ) )
+        _values[k] = static_cast<int>( k-1 );
+    }
+  }
+
+  /**
+    Queries the perversity value in a given dimension $d$. Invalid
+    dimension values only cause the function to return a zero.
+  */
+
+  int operator()( std::size_t d ) const noexcept
+  {
+    if( d < _values.size() )
+      return _values[d];
+    else
+      return 0;
+  }
+
+private:
+  std::vector<int> _values;
+};
 
 } // namespace aleph
 
