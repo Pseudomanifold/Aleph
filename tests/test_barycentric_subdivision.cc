71970e351ed45744ac507cd60889382de7e3b885
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-25 12:54:48 +0200

    Extended barycentric subdivision test case

--- tests/test_barycentric_subdivision.cc
+++ tests/test_barycentric_subdivision.cc
@@ -4,10 +4,13 @@
 #include <aleph/topology/Simplex.hh>
 #include <aleph/topology/SimplicialComplex.hh>
 
+#include <algorithm>
 #include <vector>
 
 template <class T> void test()
 {
+  ALEPH_TEST_BEGIN( "Triangle" );
+
   using DataType          = float;
   using VertexType        = T;
   using Simplex           = aleph::topology::Simplex<DataType, VertexType>;
@@ -28,6 +31,16 @@ template <class T> void test()
 
   ALEPH_ASSERT_THROW( L.empty() == false );
   ALEPH_ASSERT_THROW( K.size() < L.size() );
+
+  auto num0Simplices = std::count_if( L.begin(), L.end(), [] ( const Simplex& s ) { return s.dimension() == 0; } );
+  auto num1Simplices = std::count_if( L.begin(), L.end(), [] ( const Simplex& s ) { return s.dimension() == 1; } );
+  auto num2Simplices = std::count_if( L.begin(), L.end(), [] ( const Simplex& s ) { return s.dimension() == 2; } );
+
+  ALEPH_ASSERT_EQUAL( num0Simplices,  7 );
+  ALEPH_ASSERT_EQUAL( num1Simplices, 12 );
+  ALEPH_ASSERT_EQUAL( num2Simplices,  6 );
+
+  ALEPH_TEST_END();
 }
 
 int main(int, char**)
