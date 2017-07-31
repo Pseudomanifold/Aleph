0c70ecca6f643f60ff0b780f52d0fdc161db8d6c
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-31 13:10:36 +0200

    Implemented JSON reading for persistence diagrams
    Using the `RapidJSON` libray for this purpose because it already
    features an AUR package. I am not yet satisfied with loading and
    writing new diagrams. The interface feels somewhat clunky.

--- include/aleph/persistenceDiagrams/io/JSON.hh
+++ include/aleph/persistenceDiagrams/io/JSON.hh
@@ -1,9 +1,21 @@
 #ifndef ALEPH_PERSISTENCE_DIAGRAMS_IO_JSON_HH__
 #define ALEPH_PERSISTENCE_DIAGRAMS_IO_JSON_HH__
 
+#include <aleph/config/RapidJSON.hh>
+
+#include <aleph/persistenceDiagrams/PersistenceDiagram.hh>
+
+#include <aleph/utilities/String.hh>
+
 #include <fstream>
+#include <istream>
 #include <ostream>
 #include <string>
+
+#ifdef ALEPH_WITH_RAPID_JSON
+  #include <rapidjson/document.h>
+  #include <rapidjson/istreamwrapper.h>
+#endif
 
 namespace aleph
 {
@@ -50,6 +62,92 @@ template <class Diagram> void writeJSON( std::ostream& o, const Diagram& D, cons
   o << "\n"
     << level << "]\n"
     << "}";
+}
+
+/**
+  Convenience function for writing a persistence diagram to a file
+  in JSON format.
+*/
+
+template <class Diagram> void writeJSON( const std::string& filename, const Diagram& D, const std::string& name = std::string() )
+{
+  std::ofstream out( filename );
+  if( !out )
+    throw std::runtime_error( "Unable to open output file" );
+
+  writeJSON( out, D, name );
+}
+
+/**
+  Reads multiple persistence diagrams from an input stream in JSON
+  format. The stream is checked for consistency; appropriate error
+  messages will be raised if necessary.
+*/
+
+template <class T> std::vector< aleph::PersistenceDiagram<T> > readJSON( std::istream& in )
+{
+#ifdef ALEPH_WITH_RAPID_JSON
+
+  rapidjson::Document document;
+
+  {
+    rapidjson::IStreamWrapper isw( in );
+    document.ParseStream( isw );
+  }
+
+  using PersistenceDiagram = aleph::PersistenceDiagram<T>;
+
+  std::vector<PersistenceDiagram> persistenceDiagrams;
+
+  if( !document.HasMember( "diagrams" ) )
+    throw std::runtime_error( "Unable to find array of persistence diagrams" );
+
+  for( auto&& diagram : document["diagrams"].GetArray() )
+  {
+    auto dimension = diagram["dimension"].GetUint();
+    auto betti     = diagram["betti"].GetUint();
+    auto size      = diagram["size"].GetUint();
+
+    PersistenceDiagram persistenceDiagram;
+    persistenceDiagram.setDimension( dimension );
+
+    for( auto&& point : diagram["diagram"].GetArray() )
+    {
+      auto x  = aleph::utilities::convert<T>( point[0].GetString() );
+      auto y  = aleph::utilities::convert<T>( point[1].GetString() );
+
+      persistenceDiagram.add( x,y );
+    }
+
+    if( persistenceDiagram.size() != size )
+      throw std::runtime_error( "Stored number of points does not match number of points in persistence diagram" );
+
+    if( persistenceDiagram.betti() != betti )
+      throw std::runtime_error( "Stored Betti number does not match Betti number of persistence diagram" );
+
+    persistenceDiagrams.emplace_back( persistenceDiagram );
+  }
+
+  return persistenceDiagrams;
+
+#else
+  return {};
+#endif
+}
+
+/**
+  Reads multiple persistence diagrams from a JSON input file. This
+  is only a convenience unction provided to call the other reading
+  routine defined above.
+*/
+
+template <class T> std::vector< aleph::PersistenceDiagram<T> > readJSON( const std::string& filename )
+{
+  std::ifstream in( filename );
+  if( !in )
+    throw std::runtime_error( "Unable to read input file" );
+
+  return readJSON<T>( in );
 }
 
 } // namespace io
