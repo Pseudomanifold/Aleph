90ab6ad925a09c9ee37883f6d1aca81f02b87e87
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-31 13:15:16 +0200

    Added test & test data for JSON file reading
    So far, this only covers persistence diagram loading. I am not sure
    whether I want to support simplicial complexes as well.

---
+++ tests/test_io_json.cc
@@ -0,0 +1,27 @@
+#include <tests/Base.hh>
+
+#include <aleph/config/Base.hh>
+
+#include <aleph/persistenceDiagrams/PersistenceDiagram.hh>
+#include <aleph/persistenceDiagrams/io/JSON.hh>
+
+#include <fstream>
+#include <vector>
+
+template <class T> void testReading()
+{
+  auto filename            = CMAKE_SOURCE_DIR + std::string("/tests/input/Persistence_diagrams.json");
+  auto diagrams            = aleph::io::readJSON<T>( filename );
+
+  ALEPH_ASSERT_EQUAL( diagrams.size()    ,  5 );
+  ALEPH_ASSERT_EQUAL( diagrams[0].size() , 38 );
+  ALEPH_ASSERT_EQUAL( diagrams[0].betti(), 12 );
+  ALEPH_ASSERT_EQUAL( diagrams[3].size(),   0 );
+  ALEPH_ASSERT_EQUAL( diagrams[4].size(),   0 );
+}
+
+int main(int, char**)
+{
+  testReading<double>();
+  testReading<float >();
+}
