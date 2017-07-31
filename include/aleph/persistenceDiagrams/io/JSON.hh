64968bb76ea1a6a07fe3ebef51f4f08c46798238
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-07-31 09:48:35 +0200

    Basic JSON writing function for persistence diagrams
    This is rapidly becoming a *very* good choice for exchanging data and
    processing them with other applications, so it makes sense to prepare
    an appropriate reader function.

---
+++ include/aleph/persistenceDiagrams/io/JSON.hh
@@ -0,0 +1,59 @@
+#ifndef ALEPH_PERSISTENCE_DIAGRAMS_IO_JSON_HH__
+#define ALEPH_PERSISTENCE_DIAGRAMS_IO_JSON_HH__
+
+#include <fstream>
+#include <ostream>
+#include <string>
+
+namespace aleph
+{
+
+namespace io
+{
+
+/**
+  Writes a persistence diagram to an output stream, using the JSON
+  format. The diagram will be serialized such that its points will
+  be stored in the field ``data`` as a two-dimensional array. Note
+  that infinite values will be encoded as strings. Additional data
+  about the diagram, e.g. its dimension, are stored in name--value
+  pairs.
+*/
+
+template <class Diagram> void writeJSON( std::ostream& o, const Diagram& D, const std::string& name = std::string() )
+{
+  std::string level = "  ";
+
+  o << "{\n";
+
+  o << level << "\"betti\": "     << D.betti()     << ",\n"
+    << level << "\"dimension\": " << D.dimension() << ",\n";
+
+  if( !name.empty() )
+    o << level << "\"name\": " << name << ",\n";
+
+  o << level << "\"size\": "      << D.size()      << ",\n"
+    << level << "\"diagram\": "   << "[\n";
+
+  for( auto it = D.begin(); it != D.end(); ++it )
+  {
+    if( it != D.begin() )
+      o << ",\n";
+
+    o << level << level << "["
+                        << "\"" << it->x() << "\""
+                        << ","
+                        << "\"" << it->y() << "\""
+                        << "]";
+  }
+
+  o << "\n"
+    << level << "]\n"
+    << "}\n";
+}
+
+} // namespace io
+
+} // namespace aleph
+
+#endif
