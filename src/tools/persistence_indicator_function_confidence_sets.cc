7a060f46b4846b71d0cb0d27280b3a7bbc318520
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-01 16:19:21 +0200

    Started implementing tool for persistence indicator function confidence sets
    Using a nice bootstrap technique for empirical processes. I did not yet
    show whether this is mathematically correct, though.

---
+++ src/tools/persistence_indicator_function_confidence_sets.cc
@@ -0,0 +1,100 @@
+#include <aleph/math/Bootstrap.hh>
+
+#include <aleph/persistenceDiagrams/PersistenceDiagram.hh>
+#include <aleph/persistenceDiagrams/PersistenceIndicatorFunction.hh>
+
+#include <aleph/persistenceDiagrams/io/Raw.hh>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <cmath>
+
+using DataType                     = double;
+using PersistenceDiagram           = aleph::PersistenceDiagram<DataType>;
+using PersistenceIndicatorFunction = decltype( aleph::persistenceIndicatorFunction( PersistenceDiagram() ) );
+using Image                        = PersistenceIndicatorFunction::Image;
+
+auto meanCalculation = [] ( auto begin, auto end )
+{
+  using T  = typename std::iterator_traits<decltype(begin)>::value_type;
+  auto sum = std::accumulate( begin, end, T() );
+
+  return sum / static_cast<double>( std::distance(begin, end) );
+};
+
+// TODO: shamelessly copied from bootstrap functor class; this should
+// either be made public or the calculation below needs to be changed
+// accordingly...
+unsigned index( unsigned int samples, double alpha )
+{
+  // This accounts for rounding and works regardless of whether
+  // the product samples * alpha is an integer or not. Note the
+  // offset of -1. It is required because, say, the 100th value
+  // is at index 99 of the vector.
+  return static_cast<unsigned>( std::ceil( samples * alpha ) ) - 1;
+}
+
+int main( int argc, char** argv )
+{
+  std::vector<PersistenceIndicatorFunction> persistenceIndicatorFunctions;
+  persistenceIndicatorFunctions.reserve( argc - 1 );
+
+  for( int i = 1; i < argc; i++ )
+  {
+    std::cerr << "* Processing '" << argv[i] << "'...";
+
+    auto D = aleph::io::load<DataType>( argv[i] );
+
+    D.removeDiagonal();
+    D.removeUnpaired();
+
+    persistenceIndicatorFunctions.emplace_back( aleph::persistenceIndicatorFunction( D ) );
+
+    std::cerr << "finished\n";
+  }
+
+  // TODO: make configurable
+  unsigned numBootstrapSamples = 50;
+
+  std::vector<PersistenceIndicatorFunction> meanReplicates;
+  meanReplicates.reserve( numBootstrapSamples );
+
+  aleph::math::Bootstrap bootstrap;
+
+  bootstrap.makeReplicates( numBootstrapSamples,
+                            persistenceIndicatorFunctions.begin(), persistenceIndicatorFunctions.end(),
+                            meanCalculation,
+                            std::back_inserter( meanReplicates ) );
+
+  auto empiricalMean = meanCalculation( persistenceIndicatorFunctions.begin(), persistenceIndicatorFunctions.end() );
+
+  std::vector<Image> theta;
+  theta.reserve( numBootstrapSamples );
+
+  for( auto&& meanReplicate: meanReplicates )
+  {
+    auto n = persistenceIndicatorFunctions.size();
+    auto f = std::sqrt( n ) * ( meanReplicate - empiricalMean );
+    f      = f.abs();
+
+    theta.emplace_back( f.sup() );
+  }
+
+  std::sort( theta.begin(), theta.end() );
+
+  auto alpha    = 0.05;
+  auto quantile = theta.at( index( numBootstrapSamples, alpha / 2 ) );
+  auto fLower   = empiricalMean - quantile / std::sqrt( persistenceIndicatorFunctions.size() );
+  auto fUpper   = empiricalMean + quantile / std::sqrt( persistenceIndicatorFunctions.size() );
+
+  std::ofstream out( "/tmp/Mean_plus_confidence.txt" );
+
+  out << empiricalMean << "\n\n"
+      << fUpper        << "\n\n"
+      << fLower        << "\n";
+
+}
