fce755cb6fe163de59e17fabfb5cbfa9072b2848
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-02 09:56:43 +0200

    Implemented MLE local dimensionality estimator
    I am not quite satisfied with the results, though. The estimates in
    small neighbourhoods are *way* off.

--- include/aleph/containers/DimensionalityEstimators.hh
+++ include/aleph/containers/DimensionalityEstimators.hh
@@ -1,7 +1,10 @@
 #ifndef ALEPH_CONTAINERS_DIMENSIONALITY_ESTIMATORS_HH__
 #define ALEPH_CONTAINERS_DIMENSIONALITY_ESTIMATORS_HH__
 
+#include <stdexcept>
 #include <vector>
+
+#include <cmath>
 
 #include <aleph/math/KahanSummation.hh>
 
@@ -56,7 +59,109 @@ template <
   }
 
   return estimates;
+}
 
+/**
+  Estimates local intrinsic dimensionality of a container using its
+  nearest neighbours. No assumptions about the distribution of data
+  points are made. The function uses *maximum likelihood estimates*
+  for the dimensionality estimates.
+
+  Please see the publication
+
+    Maximum Likelihood Estimation of Intrinsic Dimension
+    Elizaveta Levina and Peter J. Bickel
+    Advances in Neural Information Processing Systems, 2005
+
+  for more details.
+
+  :param container: Container to use for dimensionality estimation
+
+  :param kMin:      Minimum number of nearest neighbours to use in
+                    computing local dimensionality estimates.
+
+  :param kMax:      Maximum number of nearest neighbours to use in
+                    computing local dimensionality estimates. This
+                    parameter influences performance.
+
+  :param distance:  Distance measure
+
+  :returns: Vector of local intrinsic dimensionality estimates that
+            are reported *without* rounding.
+*/
+
+template <
+  class Distance,
+  class Container,
+  class Wrapper
+> std::vector<double> estimateLocalDimensionalityNearestNeighboursMLE( const Container& container,
+                                                                       unsigned kMin,
+                                                                       unsigned kMax,
+                                                                       Distance /* distance */ = Distance() )
+{
+  if( kMin > kMax )
+    std::swap( kMin, kMax );
+
+  if( kMax == 0 || kMin == 0 )
+    throw std::runtime_error( "Expecting non-zero number of nearest neighbours" );
+
+  using IndexType = typename Wrapper::IndexType;
+
+  std::vector< std::vector<IndexType> > indices;
+  std::vector< std::vector<double> > distances;
+
+  Wrapper nnWrapper( container );
+  nnWrapper.neighbourSearch( kMax, indices, distances );
+
+  auto n = container.size();
+
+  std::vector<double> estimates;
+  estimates.reserve( n );
+
+  for( decltype(n) i = 0; i < n; i++ )
+  {
+    auto&& nnDistances = distances.at(i);
+
+    std::vector<double> localEstimates;
+    localEstimates.reserve( kMax );
+
+    // This follows the notation in the original paper. I dislike using
+    // $T_k$ to denote distances, though.
+    for( unsigned k = kMin - 1; k < kMax; k++ )
+    {
+      // Nothing to do here...
+      if( k == 0 )
+        continue;
+
+      std::vector<double> logEstimates;
+      logEstimates.reserve( k-1 );
+
+      for( auto it = nnDistances.begin(); it != nnDistances.begin() + k; ++it )
+      {
+        if( *it > 0.0 && nnDistances.at(k) > 0.0 )
+          logEstimates.push_back( std::log( nnDistances.at(k) / *it ) );
+
+        // This defines log(0) = 0, as usually done in information
+        // theory. The original paper does not handle this.
+        else
+          logEstimates.push_back( 0.0 );
+      }
+
+      auto mk = k > 1 ? 1.0 / (k-1) * aleph::math::accumulate_kahan( logEstimates.begin(), logEstimates.end(), 0.0 )
+                      : 0.0;
+
+      if( mk > 0.0 )
+        mk = 1.0 / mk;
+      else
+        mk = 0.0;
+
+      localEstimates.push_back( mk );
+    }
+
+    estimates.push_back( aleph::math::accumulate_kahan( localEstimates.begin(), localEstimates.end(), 0.0 ) / (kMax - kMin + 1) );
+  }
+
+  return estimates;
 }
 
 } // namespace containers
