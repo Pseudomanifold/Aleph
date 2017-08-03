ee52729926122152b706a702e149cb1c2d199e22
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-03 09:38:38 +0200

    Documented witness complex calculation algorithm; simplified interface

--- include/aleph/geometry/WitnessComplex.hh
+++ include/aleph/geometry/WitnessComplex.hh
@@ -21,16 +21,81 @@ namespace aleph
 namespace geometry
 {
 
-template <class Distance, class Container, class InputIterator> auto buildWitnessComplex(
+/**
+  Builds a witness complex from a given container. This requires a set
+  of *landmarks*. Other configuration options influence how a new edge
+  will be created from the data.
+
+  If you call this function with its barest minimum parameters by only
+  specifying a container and a set of landmarks, the resulting complex
+  will automatically adjust to your data and follows the definition of
+  the paper:
+
+    > Topological estimation using witness complexes\n
+    > Vin de Silva and Gunnar Carlsson\n
+    > Eurographics Symposium on Point-Based Graphics, 2004
+
+  The other parameters, in particular \p R, permit tuning the results,
+  thereby given the complex more "slack" when creating edges. However,
+  this also increases the size of the complex.
+
+  @param container Container for which to calculate the witness complex
+
+  @param begin     Input iterator to begin of landmark range; landmarks
+                   must be specified as *indices*. Each index has to be
+                   valid for \p container.
+
+  @param end       Input iterator to end of landmark range
+
+  @param nu        \f$\nu\f$-parameter as specified in the paper. This
+                   parameter controls which distance threshold is used
+                   for creating an edge.\n
+
+                   More precisely, an edge is created if the following
+                   holds:\n
+
+                   \f[
+                    \max(\mathrm{dist}_{a,i}\mathrm{dist}_{b,i}) \leq R + m_i
+                   \f]
+                   Here, \f$m_i\f$ refers to the \f$\nu\f$-th smallest
+                   element of the distance matrix.
+
+  @param R         Maximum radius parameter for determining whether to
+                   create an edge or not. The default value makes sure
+                   that *only* the distances between the landmarks and
+                   the points will be used for edge creation.
+
+  @param distance  Distance functor, e.g. the Euclidean distance, that
+                   is used for calculating distances between landmarks
+                   and data points. This parameter is provided to make
+                   it possible for the compiler to detect the template
+                   parameter \p distance automatically.
+
+  @returns         Witness complex of the given container. Notice that
+                   the complex is stored as a simplicial complex whose
+                   data type and index type are derived from the input
+                   data. The data of a simplex contains the *smallest*
+                   threshold for which they appear in the complex. The
+                   complex will be sorted according to this value.
+*/
+
+
+template <
+  class Distance,
+  class Container,
+  class InputIterator
+> auto buildWitnessComplex(
   const Container& container,
-  InputIterator begin, InputIterator end,
+  InputIterator begin,
+  InputIterator end,
   unsigned nu = 2,
-  typename Distance::ResultType R = typename Distance::ResultType() ) -> topology::SimplicialComplex< topology::Simplex<typename Distance::ResultType, unsigned> >
+  typename Distance::ResultType R = typename Distance::ResultType(),
+  Distance /* distance */ = Distance() ) -> topology::SimplicialComplex< topology::Simplex<typename Distance::ResultType, typename std::iterator_traits<InputIterator>::value_type> >
 {
   using IndexType         = typename std::iterator_traits<InputIterator>::value_type;
+  using VertexType        = IndexType;
   using DataType          = typename Distance::ResultType;
   using Traits            = aleph::distances::Traits<Distance>;
-  using VertexType        = unsigned; // TODO: make configurable
   using Simplex           = topology::Simplex<DataType, VertexType>;
   using SimplicialComplex = topology::SimplicialComplex<Simplex>;
 
