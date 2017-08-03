182442d6a10bdca1a1c8c997a01ce15c3e2782cb
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-03 08:41:59 +0200

    Prepared main file for library inclusion and documentation generation

---
+++ include/aleph/aleph.hh
@@ -0,0 +1,29 @@
+#ifndef ALEPH_ALEPH_HH__
+#define ALEPH_ALEPH_HH__
+
+/**
+  @namespace aleph
+  @brief     This namespace denotes the main namespace for everything in Aleph
+*/
+
+namespace aleph
+{
+  namespace geometry
+  {
+    /**
+      @namespace geometry
+      @brief     Contains functors and classes for geometrical operations
+    */
+
+  } // namespace geometry
+
+  namespace topology
+  {
+    /** @namespace topology
+        @brief     This namespace is reserved for topological concepts, such as simplicial complexes
+    */
+  } // namespace topology
+}
+
+
+#endif
