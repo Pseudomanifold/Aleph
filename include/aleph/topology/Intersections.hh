cdd9bf6cdc026cd2375c203a773feb1a3f3cc1cd
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-04 13:06:44 +0200

    Added dimensionality-constrained intersection method
    This should hopefully speed up the calculations for persistent
    intersection homology.

--- include/aleph/topology/Intersections.hh
+++ include/aleph/topology/Intersections.hh
@@ -63,6 +63,41 @@ template <class SimplicialComplex, class Simplex> std::set<Simplex> intersect( c
   return simplices;
 }
 
+/**
+  Intersects a simplex with a simplicial complex while constraining the
+  dimensionality of the intersection. The idea is that a 0-simplex will
+  only lead to 0-dimensional intersections, so there is no need to take
+  a look at higher-dimensional simplices.
+
+  Note that this assumes that the given simplicial complex is a complex
+  in the sense of mathematics---it *must* contain every face of *every*
+  simplex.
+
+  The result of this intersection is a set of simplices.
+*/
+
+template <class SimplicialComplex, class Simplex> std::set<Simplex> intersectWithConstrainedDimension( const SimplicialComplex& K, const Simplex& s ) noexcept
+{
+  static_assert( std::is_same<Simplex, typename SimplicialComplex::ValueType>::value,
+                 "Simplex type and simplicial complex value type must coincide" );
+
+  std::set<Simplex> simplices;
+
+  auto range = K.range( s.dimension() );
+
+  for( auto it = range.first; it != range.second; ++it )
+  {
+    auto&& t = *it;
+    auto u   = intersect(s,t);
+
+    // Only insert non-empty intersections
+    if( u )
+      simplices.insert( u );
+  }
+
+  return simplices;
+}
+
 } // namespace topology
 
 } // namespace aleph
