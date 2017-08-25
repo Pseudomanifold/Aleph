1a41c224abecf4c5c5b9b3d314d2c83cccd097c6
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-25 11:04:40 +0200

    Added basic test for simple HDF5 file parsing
    Did not yet check whether the results are amenable to calculating
    persistent homology. Moreover, I did not yet check different data
    types for proper conversions.

---
+++ tests/test_io_hdf5.cc
@@ -0,0 +1,43 @@
+#include <aleph/config/Base.hh>
+
+#include <tests/Base.hh>
+
+#include <aleph/topology/Simplex.hh>
+#include <aleph/topology/SimplicialComplex.hh>
+
+#include <aleph/topology/io/HDF5.hh>
+
+template <class D, class V> void test()
+{
+  ALEPH_TEST_BEGIN( "HDF5 file simple data set parsing" );
+
+  using Simplex           = aleph::topology::Simplex<D, V>;
+  using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
+
+  SimplicialComplex K;
+
+  aleph::topology::io::HDF5SimpleDataSpaceReader reader;
+  reader.setDataSetName( "Simple" );
+
+  reader( CMAKE_SOURCE_DIR + std::string( "/tests/input/Simple.h5" ),
+          K,
+          [] ( D a, D b ) { return std::min(a,b); } );
+
+  auto n0 = std::count_if( K.begin(), K.end(), [] ( const Simplex& s ) { return s.dimension() == 0; } );
+  auto n1 = std::count_if( K.begin(), K.end(), [] ( const Simplex& s ) { return s.dimension() == 1; } );
+  auto n2 = std::count_if( K.begin(), K.end(), [] ( const Simplex& s ) { return s.dimension() == 2; } );
+
+  ALEPH_ASSERT_EQUAL( n0,  9 );
+  ALEPH_ASSERT_EQUAL( n1, 16 );
+  ALEPH_ASSERT_EQUAL( n2,  8 );
+
+  ALEPH_TEST_END();
+}
+
+int main(int, char**)
+{
+  test<double,unsigned>      ();
+  test<double,unsigned short>();
+  test<float, unsigned>      ();
+  test<float, unsigned short>();
+}
