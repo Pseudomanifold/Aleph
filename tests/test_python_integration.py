ff1689dbda86ede5a394af5a73ff244b2ba60342
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-08-25 18:11:50 +0200

    Added skeleton for Python integration test file
    This still needs to be properly integrated. I am thinking of the
    following:
    
    * Set `PYTHONPATH` accordingly
    * Set up the integration test with a unit test framework that is capable
      reporting failed assertions
    * Set up an test with `CMake` to check the results of the integration

---
+++ tests/test_python_integration.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from aleph import *
+
+a   = Simplex( [0] )
+b   = Simplex( [1] )
+c   = Simplex( [2] )
+ab  = Simplex( [0,1] )
+ac  = Simplex( [0,2] )
+bc  = Simplex( [1,2] )
+abc = Simplex( [0,1,2] )
+
+K = SimplicialComplex( [a,b,c,ab,ac,bc,abc] )
+L = SimplicialComplex( [ [0], [1], [0,1] ] )
+M = SimplicialComplex( [ ([0], 0), ([1], 2), ([2], 0) ] )
+
+print(K)
+print(L)
+print(M)
+
+for vertex in abc:
+  print(vertex)
+
+for simplex in abc.boundary:
+  print(simplex)
+
+assert 0 in abc    , "Vertex not found"
+assert 3 not in abc, "Incorrect vertex"
+
+print( len(abc) )
+print( abc == abc )
+print( a.dimension )
+
+print( len(K) )
+print( len(L) )
+print( K.dimension )
+print( L.dimension )
+print( "First simplex:", K[0] )
+print( "Last simplex:", K[ len(K) -1 ] )
+
+print( "Does the complex contain the simplex?", abc in K )
+print( "Does the complex not contain the simplex?", Simplex( [4711] ) not in K )
+
+if abc:
+  print("Non-empty simplex")
+
+print( abc[2] )
+
+for diagram in calculatePersistenceDiagrams(K):
+  print(diagram)
+
+print(sorted(M, key=lambda x: x.data ))
+
+def sorting_function(s,t):
+  return s.data < t.data
+
+M.sort( sorting_function )
+
+print(M)
