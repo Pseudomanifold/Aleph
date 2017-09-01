f65f9e3f64244e0a683f47365058638803515e1e
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-09-01 17:27:02 +0200

    Implemented persistent homology calculation & basic output

--- src/tools/sparse_adjacency_matrices.cc
+++ src/tools/sparse_adjacency_matrices.cc
@@ -1,5 +1,7 @@
 #include <aleph/geometry/RipsExpander.hh>
 
+#include <aleph/persistentHomology/Calculation.hh>
+
 #include <aleph/topology/Simplex.hh>
 #include <aleph/topology/SimplicialComplex.hh>
 
@@ -8,6 +10,9 @@
 
 #include <aleph/topology/io/SparseAdjacencyMatrix.hh>
 
+#include <aleph/utilities/Format.hh>
+
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -56,7 +61,42 @@ int main( int argc, char** argv )
     K = expander.assignMaximumData( K, degrees.begin(), degrees.end() );
 
     K.sort( aleph::topology::filtrations::Data<Simplex>() );
+
+    std::cout << K << "\n";
   }
 
   std::cerr << "finished\n";
+
+  // Calculate persistent homology -------------------------------------
+
+  {
+    std::size_t index = 0;
+
+    for( auto&& K : simplicialComplexes )
+    {
+      bool dualize                    = true;
+      bool includeAllUnpairedCreators = true;
+
+      auto diagrams
+        = aleph::calculatePersistenceDiagrams( K,
+                                               dualize,
+                                               includeAllUnpairedCreators );
+
+      for( auto&& diagram : diagrams )
+      {
+        diagram.removeDiagonal();
+
+        auto output = "/tmp/"
+                      + aleph::utilities::format( index, simplicialComplexes.size() )
+                      + "_d"
+                      + std::to_string( diagram.dimension() )
+                      + ".txt";
+
+        std::ofstream out( output );
+        out << diagram << "\n";
+      }
+
+      ++index;
+    }
+  }
 }
