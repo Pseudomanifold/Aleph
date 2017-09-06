4a383d98452a2b2a1fc3b938eb46bf5b0a208cb1
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-09-06 18:04:53 +0200

    Naive beta-skeleton calculation
    Added a rather naive way of calculating beta-skeletons for containers
    such as point clouds. Nothing is optimized so far.

---
+++ include/aleph/geometry/BetaSkeleton.hh
@@ -0,0 +1,162 @@
+#ifndef ALEPH_GEOMETRY_BETA_SKELETON_HH__
+#define ALEPH_GEOMETRY_BETA_SKELETON_HH__
+
+#include <aleph/geometry/distances/Traits.hh>
+
+#include <aleph/utilities/ContainerOperators.hh>
+
+#include <aleph/topology/Simplex.hh>
+#include <aleph/topology/SimplicialComplex.hh>
+
+#include <vector>
+
+namespace aleph
+{
+
+namespace geometry
+{
+
+namespace detail
+{
+
+/*
+  Describes a d-dimensional ball with a certain diameter and a certain
+  centre; the ball will be used during beta-skeleton construction.
+*/
+
+class BetaBall
+{
+public:
+
+  // This is required because the lune cannot initialize its balls prior
+  // to calculating certain distances in the point cloud.
+  BetaBall()
+  {
+  }
+
+  template <class T> BetaBall( const std::vector<T>& centre, double diameter )
+    : _centre( centre.begin(), centre.end() )
+    , _radius( 0.5 * diameter )
+  {
+  }
+
+  template <class T > bool contains( const std::vector<T>& other ) const
+  {
+    double distance = 0.0;
+
+    for( std::size_t i = 0; i < _centre.size(); i++ )
+      distance += ( _centre[i] - other[i] ) * ( _centre[i] - other[i] );
+
+    return distance <= _radius * _radius;
+  }
+
+private:
+  std::vector<double> _centre;
+  double _radius = 0.0;
+};
+
+/*
+  Describes the lune that is used as the empty region of the beta-skeleton
+  and offers a way of checking for the presence of data points.
+*/
+
+template <class Container, class Index = std::size_t> class BetaLune
+{
+public:
+  BetaLune( const Container& container,
+            Index p,
+            Index q,
+            double beta,
+            double d )
+    : _container( container )
+  {
+    using namespace aleph::utilities;
+
+    auto&& P      = container[p];
+    auto&& Q      = container[q];
+    auto centreP  = (1.0-0.5*beta) * P + 0.5*beta * Q;
+    auto centreQ  = (1.0-0.5*beta) * Q + 0.5*beta * P;
+    auto diameter =  beta * d;
+
+    _pBall = BetaBall( centreP, diameter );
+    _qBall = BetaBall( centreQ, diameter );
+  }
+
+  bool contains( Index r ) const
+  {
+    return _pBall.contains( _container[r] )
+        && _qBall.contains( _container[r] );
+  }
+
+private:
+  const Container& _container;
+
+  BetaBall _pBall;
+  BetaBall _qBall;
+};
+
+} // namespace detail
+
+template <class Distance, class Container, class Index = std::size_t>
+  auto buildBetaSkeletonNaive( const Container& container,
+                               double beta,
+                               Distance distance = Distance() )
+    -> topology::SimplicialComplex< topology::Simplex<typename Distance::ResultType, Index> >
+
+{
+  using Traits            = aleph::distances::Traits<Distance>;
+  using DataType          = typename Distance::ResultType;
+  using VertexType        = Index;
+  using Simplex           = topology::Simplex<DataType, VertexType>;
+  using SimplicialComplex = topology::SimplicialComplex<Simplex>;
+
+  auto n = container.size();
+  auto d = container.dimension();
+
+  Traits traits;
+  SimplicialComplex betaSkeleton;
+
+  for( Index i = 0; i < n; i++ )
+    betaSkeleton.push_back( Simplex(i) );
+
+  for( Index i = 0; i < n; i++ )
+  {
+    auto&& p = container[i];
+
+    for( Index j = i+1; j < container.size(); j++ )
+    {
+      auto&& q  = container[j];
+      auto dist = traits.from( distance( p.begin(), q.begin(), d ) );
+
+      using namespace detail;
+
+      BetaLune<Container> lune( container,
+                                i,
+                                j,
+                                beta,
+                                dist );
+
+      bool addEdge = true;
+
+      for( Index r = 0; r < container.size(); r++ )
+      {
+        if( r != i && r != j && lune.contains( r ) )
+        {
+          addEdge = false;
+          break;
+        }
+      }
+
+      if( addEdge )
+        betaSkeleton.push_back( Simplex( {i,j}, dist ) );
+    }
+  }
+
+  return betaSkeleton;
+}
+
+} // namespace geometry
+
+} // namespace aleph
+
+#endif
---
+++ tests/test_beta_skeleton.cc
@@ -0,0 +1,35 @@
+#include <aleph/config/Base.hh>
+
+#include <tests/Base.hh>
+
+#include <aleph/containers/PointCloud.hh>
+
+#include <aleph/geometry/BetaSkeleton.hh>
+
+#include <aleph/geometry/distances/Euclidean.hh>
+
+using namespace aleph::containers;
+using namespace aleph::geometry;
+using namespace aleph::distances;
+
+template <class T> void test()
+{
+  using PointCloud = PointCloud<T>;
+  using Distance   = Euclidean<T>;
+
+  PointCloud pc = load<T>( CMAKE_SOURCE_DIR + std::string("/tests/input/Iris_tab_separated.txt") );
+
+  auto K0 = buildBetaSkeletonNaive( pc, 0, Distance() );
+  auto K1 = buildBetaSkeletonNaive( pc, 1, Distance() );
+  auto K2 = buildBetaSkeletonNaive( pc, 2, Distance() );
+  auto K3 = buildBetaSkeletonNaive( pc, 3, Distance() );
+
+  for( auto&& K : {K0,K1,K2,K3} )
+    ALEPH_ASSERT_THROW( K.empty() == false );
+}
+
+int main( int, char** )
+{
+  test<float> ();
+  test<double>();
+}
