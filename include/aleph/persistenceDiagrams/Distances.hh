e6265389648fcac05afad280c4d45f6cd4f5f90d
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-09-20 08:41:22 +0200

    Added meta file for persistence diagram distances

---
+++ include/aleph/persistenceDiagrams/Distances.hh
@@ -0,0 +1,14 @@
+#ifndef ALEPH_PERSISTENCE_DIAGRAMS_DISTANCES_HH__
+#define ALEPH_PERSISTENCE_DIAGRAMS_DISTANCES_HH__
+
+/*
+  This is a meta file for including *all* distance functors for
+  persistence diagrams.
+*/
+
+#include <distances/Bottleneck.hh>
+#include <distances/Hausdorff.hh>
+#include <distances/NearestNeighbour.hh>
+#include <distances/Wasserstein.hh>
+
+#endif
