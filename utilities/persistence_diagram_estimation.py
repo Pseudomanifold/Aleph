d410d1307ec429e62df63e7255333338ff91f044
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-09-20 10:12:09 +0200

    Prior range estimates for distribution parameters

--- utilities/persistence_diagram_estimation.py
+++ utilities/persistence_diagram_estimation.py
@@ -18,6 +18,20 @@ def point_estimates(samples):
 def pdf(x, alpha, beta):
   return stats.gamma.pdf(x, a=alpha, scale=1.0/beta)
 
+def make_prior_ranges(alpha, beta, n, N, k=3):
+  mean    = alpha/beta
+  var     = alpha/beta**2
+  se_mean = mean / math.sqrt(n)
+  se_var  = math.sqrt(2*var**2 / (n-1))
+
+  means   = numpy.linspace(mean - se_mean * k, mean + se_mean * k, N)
+  vars    = numpy.linspace(var - se_var * k, var + se_var * k, N)
+
+  betas    = [ mean / var for mean,sigma in zip(means,vars) ]
+  alphas   = [ mean**2 / var for mean,sigma in zip(means,vars) ]
+
+  return alphas, betas
+
 if __name__ == "__main__":
   filename = sys.argv[1]
   data     = list()
@@ -32,24 +46,37 @@ if __name__ == "__main__":
       if not math.isinf(c) and not math.isinf(d):
         data.append( (c,d) )
 
-  alpha_c, beta_c = point_estimates( [ c for c,_ in data ] )
-  alpha_d, beta_d = point_estimates( [ d for _,d in data ] )
+  c_alpha, c_beta = point_estimates( [ c for c,_ in data ] )
+  d_alpha, d_beta = point_estimates( [ d for _,d in data ] )
 
   # Plot the distributions to ensure that the point estimates are
   # actually useful.
 
-  v = [ c for c,_ in data ]
-  x = numpy.linspace(min(v), max(v), 100)
+  if False:
+    v = [ c for c,_ in data ]
+    x = numpy.linspace(min(v), max(v), 100)
 
-  plt.hist([c for c,_ in data], normed=True, bins=20, label="Creation [samples]")
-  plt.plot(x, pdf(x, alpha_c, beta_c), label="Creation [estimate]")
-  plt.legend()
-  plt.show()
+    plt.hist([c for c,_ in data], normed=True, bins=20, label="Creation [samples]")
+    plt.plot(x, pdf(x, d_alpha, c_beta), label="Creation [estimate]")
+    plt.legend()
+    plt.show()
 
-  v = [ d for _,d in data ]
-  x = numpy.linspace(min(v), max(v), 100)
+    v = [ d for _,d in data ]
+    x = numpy.linspace(min(v), max(v), 100)
 
-  plt.hist([c for c,_ in data], normed=True, bins=20, label="Destruction [samples]")
-  plt.plot(x, pdf(x, alpha_d, beta_d), label="Destruction [estimate]")
-  plt.legend()
-  plt.show()
+    plt.hist([c for c,_ in data], normed=True, bins=20, label="Destruction [samples]")
+    plt.plot(x, pdf(x, d_alpha, d_beta), label="Destruction [estimate]")
+    plt.legend()
+    plt.show()
+
+  c_alphas, c_betas = make_prior_ranges(c_alpha, c_beta, len(data), 100)
+
+  #v = [ c for c,_ in data ]
+  #x = numpy.linspace(min(v), max(v), 100)
+
+  #plt.hist([c for c,_ in data], normed=True, bins=20)
+
+  #for c_alpha, c_beta in zip(c_alphas, c_betas):
+  #  plt.plot(x, pdf(x, c_alpha, c_beta))
+
+  #plt.show()
