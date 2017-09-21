f5aa84579ce3fba4cba9ab008e7cddeb40ec22eb
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-09-21 11:22:52 +0200

    Preparing network analysis example for Python bindings
    * Basic functionality (filename selection, calculation) works
    * No support for Rips expansion yet

---
+++ examples/python/network_analysis.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#
+# This is an examnple file shipped by 'Aleph - A Library for Exploring
+# Persistent Homology'.
+#
+# This example demonstrates how to load a network, i.e. a graph, from
+# a variety of input files. The graph will subsequently be *expanded*
+# to a simplicial complex, whose persistent homology is calculated.
+#
+# This example demonstrates the Python interface of Aleph. Note that the
+# functionality presented in this file should be roughly equivalent to:
+#
+#   examples/network_analysis.cc
+#
+# Some deviations between the C++ interface and the Python interface are
+# allowed and to be expected.
+#
+# Original author: Bastian Rieck
+
+from aleph import *
+
+import argparse
+import sys
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser() 
+  parser.add_argument("-i", "--invert-weights", action="store_true", help="if specified, inverts weights stored in the network")
+  parser.add_argument("FILE", type=str, help="input file; must contain a *valid* network that can be read by Aleph")
+
+  arguments = parser.parse_args()
+  filename  = arguments.FILE
+
+  sys.stderr.write("Loading '%s'..." % filename)
+  sys.stderr.flush()
+
+  K = load(filename)
+
+  sys.stderr.write("finished\n")
+
+  # TODO:
+  #  - Rips expansion
+  #  - Weight assignment
+
+  diagrams = calculatePersistenceDiagrams( K )
+
+  for diagram in diagrams:
+    diagram.removeDiagonal()
+
+    for point in diagram:
+      print(point)
+
+    print(diagram)
