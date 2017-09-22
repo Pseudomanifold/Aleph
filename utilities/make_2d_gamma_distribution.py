6f5124569e747545d7a3144b18f7e41902aaccbd
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-09-22 18:14:19 +0200

    Demo script for creating a 2D Gamma distribution
    Technically, this creates *two* different Gamma distributions that are
    used to determine the point coordinates.

---
+++ utilities/make_2d_gamma_distribution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#
+# This file is part of 'Aleph - A Library for Exploring Persistent
+# Homology'. It creates a set of points in the plane, whose values
+# follow two different Gamma distributions.
+
+import numpy as np
+
+if __name__ == "__main__":
+  n      = 20
+  alphas = [7.5,9.0]
+  betas  = [1.0,2.0]
+
+  for i in range(n):
+    for j in range(n):
+      x = np.random.gamma(alphas[0], 1.0 / betas[0])
+      y = np.random.gamma(alphas[1], 1.0 / betas[1])
+
+      print(x,y)
