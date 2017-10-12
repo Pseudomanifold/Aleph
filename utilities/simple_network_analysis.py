7d71df96fac9b36f196bfad117eb913ab42e0ca4
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-10-12 13:11:53 +0200

    Added classifiers (with cross-validation)
    Following the experimental setup in the paper "Deep Graph Kernels" by
    Yanardag & Vishwanathan.

--- utilities/simple_network_analysis.py
+++ utilities/simple_network_analysis.py
@@ -9,6 +9,12 @@
 #
 # Original author: Bastian Rieck
 
+from sklearn.linear_model    import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm             import LinearSVC
+from sklearn.tree            import DecisionTreeClassifier
+
 import numpy    as np
 import networkx as nx
 import pandas   as pd
@@ -46,13 +52,13 @@ if __name__ == "__main__":
   for G in graphs:
     average_clustering_coefficient = nx.average_clustering(G)
     average_degree                 = np.mean( [degree for _,degree in nx.degree(G) ] )
+    average_shortest_path_length   = 0
+    diameter                       = 0
 
-    average_shortest_path_length = 0
-    diameter                     = 0
-
-    for g in nx.connected_component_subgraphs(G):
-      average_shortest_path_length = max(average_shortest_path_length, nx.average_shortest_path_length(g))
-      diameter                     = max(diameter, nx.diameter(g))
+    if False:
+      for g in nx.connected_component_subgraphs(G, copy=False):
+        average_shortest_path_length = max(average_shortest_path_length, nx.average_shortest_path_length(g))
+        diameter                     = max(diameter, nx.diameter(g))
 
     density = nx.density(G)
     data    = {
@@ -64,3 +70,15 @@ if __name__ == "__main__":
     }
 
     df = df.append(data, ignore_index=True)
+
+  ######################################################################
+  # Perform classification
+  ######################################################################
+
+  X = df.values
+  y = labels[:len(graphs)]
+
+  classifiers = [ DecisionTreeClassifier(), LinearSVC(), LogisticRegression() ]
+  for clf in classifiers:
+    scores = cross_val_score(clf, X, y, cv=10)
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
