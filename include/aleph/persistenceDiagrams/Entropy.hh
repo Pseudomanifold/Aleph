75a97d23db3852e990ee87509bfbb90c75397bd6
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-10-17 08:35:58 +0200

    Basic implementation of nearest-neighbour area entropy
    This is still copied almost verbatim from the corresponding entropy test
    tool. The old code needs removal as soon as this implementation works.

--- include/aleph/persistenceDiagrams/Entropy.hh
+++ include/aleph/persistenceDiagrams/Entropy.hh
@@ -3,6 +3,11 @@
 
 #include <aleph/containers/PointCloud.hh>
 
+#include <aleph/geometry/BruteForce.hh>
+#include <aleph/geometry/FLANN.hh>
+
+#include <aleph/geometry/distances/Euclidean.hh>
+
 #include <aleph/persistenceDiagrams/PersistenceDiagram.hh>
 
 #include <aleph/math/KahanSummation.hh>
@@ -50,6 +55,14 @@ template <class T> aleph::containers::PointCloud<T> makePointCloud( const aleph:
   return pc;
 }
 
+template <class T> T log2( T x )
+{
+  if( x == T() )
+    return T();
+  else
+    return std::log2( x );
+}
+
 } // namespace detail
 
 /**
@@ -92,6 +105,67 @@ template <class T> T persistentEntropy( const aleph::PersistenceDiagram<T>& D )
                     auto p = persistence / totalPersistence;
                     return p * std::log2( p );
                   } );
+}
+
+template <class T> T nearestNeighbourAreaEntropy( const aleph::PersistenceDiagram<T>& diagram )
+{
+  using PointCloud = aleph::containers::PointCloud<T>;
+  using Distance   = aleph::geometry::distances::Euclidean<T>;
+
+#ifdef ALEPH_WITH_FLANN
+  using NearestNeighbours = aleph::geometry::FLANN<PointCloud, Distance>;
+#else
+  using NearestNeighbours = aleph::geometry:::BruteForce<PointCloud, Distance>;
+#endif
+
+  auto pc = detail::makePointCloud( diagram );
+  NearestNeighbours nn( pc );
+
+  using ElementType = typename NearestNeighbours::ElementType;
+  using IndexType   = typename NearestNeighbours::IndexType;
+
+  std::vector< std::vector<IndexType> > indices;
+  std::vector< std::vector<ElementType> > distances;
+
+  nn.neighbourSearch( 2,  // the 1st nearest neighbour is the point itself
+                      indices,
+                      distances );
+
+  std::vector<ElementType> nearestNeigbourDistances;
+  nearestNeigbourDistances.reserve( pc.size() );
+
+  for( auto&& distance : distances )
+    nearestNeigbourDistances.push_back( distance.at(1) );
+
+  std::vector<ElementType> areas( pc.size() );
+
+  std::transform( nearestNeigbourDistances.begin(), nearestNeigbourDistances.end(),
+                  areas.begin(),
+                  [] ( ElementType radius )
+                  {
+                    return static_cast<ElementType>( radius * radius * 2 * M_PI );
+                  } );
+
+  auto totalArea
+    = aleph::math::accumulate_kahan_sorted( areas.begin(), areas.end(),
+                                            ElementType() );
+
+  std::vector<T> entropies( pc.size() );
+
+  std::transform( areas.begin(), areas.end(),
+                  entropies.begin(),
+                  [&totalArea] ( ElementType area )
+                  {
+                    T p = static_cast<T>( area / totalArea );
+                    T e = p * detail::log2( p );
+
+                    return e;
+                  } );
+
+  return -aleph::math::accumulate_kahan_sorted( entropies.begin(),
+                                                entropies.end(),
+                                                T() );
+
 }
 
 } // namespace aleph
