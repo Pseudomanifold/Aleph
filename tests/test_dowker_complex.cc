74506794fc95e954e8a864231fdc03cb4689addd
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-10-23 15:40:03 +0200

    Fixed weights
    By slightly changing the weights, I am ensuring that the calculation now
    works as expected. The example given in the paper appears to be wrong.

--- tests/test_dowker_complex.cc
+++ tests/test_dowker_complex.cc
@@ -16,13 +16,13 @@ template <class T> void test()
   X[0][2] = T(4);
   X[1][0] = T(1);
   X[1][2] = T(5);
-  X[2][0] = T(2);
+  X[2][0] = T(3);
   X[2][1] = T(3);
 
   Matrix Y( 3, std::vector<T>( 3 ) );
 
   Y[0][1] = T(6);
-  Y[0][2] = T(2);
+  Y[0][2] = T(3);
   Y[1][0] = T(1);
   Y[1][2] = T(5);
   Y[2][0] = T(4);
@@ -45,7 +45,6 @@ template <class T> void test()
   auto Y_sink   = Y_dowkerComplexes.second;
 
   std::cerr << X_source << "\n" << Y_source << "\n";
-
 
   ALEPH_TEST_END();
 }
