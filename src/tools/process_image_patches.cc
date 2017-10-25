f4fb840420940cca59cc46af1e9f9ae0c141a464
Bastian Rieck <bastian@rieck.ru>
2017-10-25 15:26:07 +0200

    Added norm-based filtering for image patches

--- src/tools/process_image_patches.cc
+++ src/tools/process_image_patches.cc
@@ -89,6 +89,9 @@ int main( int argc, char** argv )
   using IndexType                = decltype( pointCloud.size() );
   PointCloud processedPointCloud = PointCloud( pointCloud.size(), pointCloud.dimension() );
 
+  std::vector<DataType> contrastNorms;
+  contrastNorms.reserve( pointCloud.size() );
+
   for( IndexType i = 0; i < pointCloud.size(); i++ )
   {
     auto p = pointCloud[i];
@@ -109,6 +112,7 @@ int main( int argc, char** argv )
                     } );
 
     auto norm = contrastNorm( p.begin(), p.end() );
+    contrastNorms.push_back( norm );
 
     if( norm > DataType() )
     {
@@ -120,5 +124,41 @@ int main( int argc, char** argv )
     }
 
     processedPointCloud.set( i, p.begin(), p.end() );
+  }
+
+  // Filter patches based on norm --------------------------------------
+  //
+  // In the original paper, only the top 20% of the contrast norms are
+  // being kept. This tool uses a configurable threshold.
+
+  {
+    auto contrastNorms_          = contrastNorms;
+    double contrastNormThreshold = 0.20; // TODO: make configurable
+
+    std::sort( contrastNorms_.begin(),
+               contrastNorms_.end() );
+
+    auto threshold = contrastNorms_.at( std::size_t( std::ceil( (1.0 - contrastNormThreshold) * double( contrastNorms.size() ) ) ) );
+
+    auto numRemainingPatches
+      = std::count_if( contrastNorms.begin(), contrastNorms.end(),
+                       [&threshold] ( DataType norm )
+                       {
+                         return norm >= threshold;
+                       } );
+
+    PointCloud filteredPointCloud( numRemainingPatches, pointCloud.dimension() );
+    IndexType j = 0;
+
+    for( std::size_t i = 0; i < contrastNorms.size(); i++ )
+    {
+      if( contrastNorms.at(i) >= threshold )
+      {
+        auto p = processedPointCloud[i];
+        filteredPointCloud.set( j++, p.begin(), p.end() );
+      }
+    }
+
+    swap( processedPointCloud, filteredPointCloud );
   }
 }
