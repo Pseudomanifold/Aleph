6abb9dbd524da33de31ca030cae1428674dcd357
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-06 15:41:10 +0100

    Extended tangent space test for circles
    Require an example for which I know the expected values of the curvature
    estimates.

--- tests/test_tangent_space.cc
+++ tests/test_tangent_space.cc
@@ -6,9 +6,38 @@
 
 #include <iostream>
 
+#include <cmath>
+
 using namespace aleph;
 using namespace containers;
 using namespace geometry;
+
+template <class T> void testCircle()
+{
+  auto n = 100;
+  auto k = 5;
+
+  // FIXME: use for tangent space estimation
+  (void) k;
+
+  using PointCloud = PointCloud<T>;
+  PointCloud pc( n, 2 );
+
+  for( int i = 0; i < n; i++ )
+  {
+    auto phi = T(2*M_PI*i/n);
+    auto r   = T(1.0);
+    auto x   = r * std::cos( phi );
+    auto y   = r * std::sin( phi );
+
+    pc.set(i, {x,y} );
+  }
+
+#ifdef ALEPH_WITH_EIGEN
+  TangentSpace ts;
+  ts( pc );
+#endif
+}
 
 template <class T> void test()
 {
@@ -24,6 +53,9 @@ template <class T> void test()
 
 int main( int, char** )
 {
-  test<float> ();
-  test<double>();
+  testCircle<float> ();
+  testCircle<double>();
+
+  //test<float> ();
+  //test<double>();
 }
