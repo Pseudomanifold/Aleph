8ed1daaae035e2473ff101d7ebf0f45068eaa67e
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-08 09:16:40 +0100

    Check for admissible and principal simplices

--- include/aleph/topology/Spine.hh
+++ include/aleph/topology/Spine.hh
@@ -3,11 +3,18 @@
 
 #include <aleph/topology/Intersections.hh>
 
+#include <vector>
+
 namespace aleph
 {
 
 namespace topology
 {
+
+/**
+  Checks whether a simplex in a simplicial complex is principal, i.e.
+  whether it is not a proper face of any other simplex in K.
+*/
 
 template <class SimplicialComplex, class Simplex> bool isPrincipal( const Simplex& s, const SimplicialComplex& K )
 {
@@ -15,7 +22,9 @@ template <class SimplicialComplex, class Simplex> bool isPrincipal( const Simple
 
   for( auto&& t : K )
   {
-    if( s.dimension() < t.dimension() )
+    // This check assumes that the simplicial complex is valid, so it
+    // suffices to search faces in one dimension _below_ s..
+    if( s.dimension()+1 == t.dimension() )
     {
       if( intersect(s,t) == s )
         principal = false;
@@ -23,15 +32,42 @@ template <class SimplicialComplex, class Simplex> bool isPrincipal( const Simple
   }
 
   return principal;
-
 }
+
+/**
+  Checks whether a simplex in a simplicial complex is admissible, i.e.
+  the simplex is *principal* and has at least one free face.
+*/
 
 template <class SimplicialComplex, class Simplex> bool isAdmissible( const Simplex& s, const SimplicialComplex& K )
 {
-  bool admissible = true;
+  if( !isPrincipal(s,K) )
+    return false;
 
-  for( auto&& t : K )
+  // Check whether a free face exists ----------------------------------
+
+  std::vector<Simplex> faces( s.begin_boundary(), s.end_boundary() );
+  std::vector<bool> admissible( faces.size(), false );
+
+  std::size_t i = 0;
+
+  for( auto&& face : faces )
   {
+    for( auto&& t : K )
+    {
+      // This check assumes that the simplicial complex is valid, so it
+      // suffices to search faces in one dimension _below_ t.
+      if( face.dimension()+1 == t.dimension() && t != s )
+      {
+        if( intersect(face,t) == face )
+        {
+          admissible[i] = false;
+          break;
+        }
+      }
+    }
+
+    ++i;
   }
 
   return admissible;
