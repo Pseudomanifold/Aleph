ec9dd5c5c29c975bf870bef2c37dd1e58399fbc9
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-08 10:13:40 +0100

    Basic unit test for topological spines

---
+++ tests/test_spine.cc
@@ -0,0 +1,32 @@
+#include <tests/Base.hh>
+
+#include <aleph/topology/Simplex.hh>
+#include <aleph/topology/SimplicialComplex.hh>
+#include <aleph/topology/Spine.hh>
+
+#include <vector>
+
+template <class T> void testTriangle()
+{
+  using DataType   = bool;
+  using VertexType = T;
+
+  using Simplex           = aleph::topology::Simplex<DataType, VertexType>;
+  using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
+
+  SimplicialComplex K = {
+    {0,1,2},
+    {0,1}, {0,2}, {1,2},
+    {0}, {1}, {2}
+  };
+
+  auto L = aleph::topology::spine( K );
+
+  ALEPH_ASSERT_THROW( L.size() < K.size() );
+}
+
+int main( int, char** )
+{
+  testTriangle<short>   ();
+  testTriangle<unsigned>();
+}
