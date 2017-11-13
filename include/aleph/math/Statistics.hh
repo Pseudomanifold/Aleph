feda80e5086935be0072f4714651af3cbcc29a55
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-13 10:03:23 +0100

    Extended implementation of statistical functions

--- include/aleph/math/Statistics.hh
+++ include/aleph/math/Statistics.hh
@@ -1,10 +1,9 @@
 #ifndef ALEPH_MATH_STATISTICS_HH__
 #define ALEPH_MATH_STATISTICS_HH__
 
-#include <aleph/math/Quantiles.hh>
-
 #include <iterator>
 #include <limits>
+#include <numeric>
 
 #include <cmath>
 
@@ -13,6 +12,24 @@ namespace aleph
 
 namespace math
 {
+
+/**
+  Given a range of numbers, calculates the sample mean of the data.
+
+  @param begin Input iterator to begin of range
+  @param end   Input iterator to end of range
+
+  @returns Sample mean
+*/
+
+template <class InputIterator> double sampleMean( InputIterator begin,
+                                                  InputIterator end )
+{
+  if( begin == end )
+    return std::numeric_limits<double>::quiet_NaN();
+
+  return std::accumulate( begin, end, 0.0 ) / static_cast<double>( std::distance( begin, end ) );
+}
 
 /**
   Given a range of numbers, calculates the sample variance of the data.
@@ -32,7 +49,7 @@ template <class InputIterator> double sampleVariance( InputIterator begin,
     return( std::numeric_limits<double>::quiet_NaN() );
   }
 
-  double mean = math::mean( begin, end );
+  double mean = sampleMean( begin, end );
   double sum  = 0.0;
 
   for( InputIterator it = begin; it != end; ++it )
@@ -58,6 +75,73 @@ template <class InputIterator> double sampleStandardDeviation( InputIterator beg
                                                                InputIterator end )
 {
   return std::sqrt( sampleVariance( begin, end ) );
+}
+
+/**
+  Given a range of numbers, calculates the sample skewness of the data.
+
+  @param begin Input iterator to begin of range
+  @param end   Input iterator to end of range
+
+  @returns Sample skewness
+*/
+
+template <class InputIterator> double sampleSkewness( InputIterator begin,
+                                                      InputIterator end )
+{
+  if( begin == end )
+    std::numeric_limits<double>::quiet_NaN();
+
+  double mean        = sampleMean( begin, end );
+  double numerator   = 0.0;
+  double denominator = 0.0;
+
+  for( InputIterator it = begin; it != end; ++it )
+  {
+    numerator   += std::pow( static_cast<double>( *it - mean ), 3.0 );
+    denominator += ( *it - mean ) * ( *it - mean );
+  }
+
+  numerator   /= static_cast<double>( std::distance( begin, end ) );
+
+  denominator /= static_cast<double>( std::distance( begin, end ) );
+  denominator  = std::pow( denominator, 3.0 / 2.0 );
+
+  return numerator / denominator;
+}
+
+/**
+  Given a range of numbers, calculates the sample excess kurtosis of the
+  data.
+
+  @param begin Input iterator to begin of data
+  @param end   Input iterator to end of data
+
+  @returns Sample excess kurtosis
+*/
+
+template <class InputIterator> double sampleKurtosis( InputIterator begin,
+                                                      InputIterator end )
+{
+  if( begin == end )
+    return std::numeric_limits<double>::quiet_NaN();
+
+  double mean = sampleMean( begin, end );
+  double m4   = 0.0;
+  double m2   = 0.0;
+
+  double n    = static_cast<double>( std::distance( begin, end ) );
+
+  for( InputIterator it = begin; it != end; ++it )
+  {
+    m4 += std::pow( static_cast<double>( *it - mean ), 4.0 );
+    m2 += std::pow( static_cast<double>( *it - mean ), 2.0 );
+  }
+
+  m4 /= n;
+  m2 /= n;
+
+  return m4 / (m2 * m2 ) - 3.0;
 }
 
 } // namespace math
