2764be29add100720bf56d3e0691de8012073636
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-14 15:00:34 +0100

    Started implementing tool for local dimensionality estimation

---
+++ src/tools/local_dimensionality.cc
@@ -0,0 +1,85 @@
+/*
+  This is a tool shipped by 'Aleph - A Library for Exploring Persistent
+  Homology'.
+
+  Given an input point cloud, it performs local dimensionality
+  estimation (using different schmes) and stores the estimates
+  along with the original point cloud.
+*/
+
+#include <aleph/containers/DimensionalityEstimators.hh>
+#include <aleph/containers/PointCloud.hh>
+
+#include <aleph/geometry/FLANN.hh>
+#include <aleph/geometry/BruteForce.hh>
+
+#include <aleph/geometry/distances/Euclidean.hh>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <getopt.h>
+
+using DataType   = double;
+using PointCloud = aleph::containers::PointCloud<DataType>;
+using Distance   = aleph::geometry::distances::Euclidean<DataType>;
+
+#ifdef ALEPH_WITH_FLANN
+  using NearestNeighbours = aleph::geometry::FLANN<PointCloud, Distance>;
+#else
+  using NearestNeighbours = aleph::geometry::BruteForce<PointCloud, Distance>;
+#endif
+
+int main( int argc, char** argv )
+{
+  std::string method = "pca";
+  unsigned k         = 8;
+
+  {
+    static option commandLineOptions[] =
+    {
+      { "k"          , required_argument, nullptr, 'k' },
+      { "method"     , required_argument, nullptr, 'm' },
+      { nullptr      , 0                , nullptr,  0  }
+    };
+
+    int option = 0;
+    while( ( option = getopt_long( argc, argv, "k:m:", commandLineOptions, nullptr ) ) != -1 )
+    {
+      switch( option )
+      {
+      case 'k':
+        k = static_cast<unsigned>( std::stoull( optarg ) );
+        break;
+      case 'm':
+        method = optarg;
+        break;
+      }
+    }
+  }
+
+  if( ( argc - optind ) <= 1 )
+    return -1;
+
+  std::string filename = argv[ optind++ ];
+
+  std::cerr << "* Loading point cloud from '" << filename << "'...";
+
+  PointCloud pc = aleph::containers::load<DataType>( filename );
+
+  std::cerr << "finished\n"
+            << "* Loaded point cloud with " << pc.size() << " points of dimension " << pc.dimension() << "\n";
+
+  std::vector<double> dimensionalities;
+
+  if( method == "pca" )
+  {
+    auto result
+      = aleph::containers::estimateLocalDimensionalityPCA<Distance, PointCloud, NearestNeighbours>( pc, k );
+
+    // converts data in the vector so that we don't have to do it
+    // manually
+    dimensionalities.assign( result.begin(), result.end() );
+  }
+}
