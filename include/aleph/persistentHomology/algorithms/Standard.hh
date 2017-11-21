33a827d6a43d0f88fc3ae3792dbf948fc9de2be6
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-21 08:48:30 +0100

    Providing additional standard reduction algorithm for rectangular matrices
    I am little bit torn concerning a proper solution for this issue. On the
    one hand, I do not want to waste cycles by adding an additional check as
    for quadratic matrices, this is *not* necessary. On the other hand, code
    duplication is not nice as well.
    
    For now, leaving this as-is and re-implementing the reduction schemes in
    order to finally get dual matrices to work.

--- include/aleph/persistentHomology/algorithms/Standard.hh
+++ include/aleph/persistentHomology/algorithms/Standard.hh
@@ -45,6 +45,40 @@ public:
   }
 };
 
+class StandardRectangular
+{
+public:
+  template <class Representation> void operator()( topology::BoundaryMatrix<Representation>& M )
+  {
+    using Index = typename Representation::Index;
+
+    auto numColumns = M.getNumColumns();
+
+    std::vector< std::pair<Index, bool> > lut( static_cast<std::size_t>( numColumns ),
+                                               std::make_pair(0, false) );
+
+    for( Index j = 0; j < numColumns; j++ )
+    {
+      Index i;
+      bool valid = false;
+
+      std::tie( i, valid ) = M.getMaximumIndex( j );
+
+      if( i >= numColumns )
+        continue;
+
+      while( valid && lut[ static_cast<std::size_t>(i) ].second )
+      {
+        M.addColumns( lut[ static_cast<std::size_t>(i) ].first, j );
+        std::tie( i, valid ) = M.getMaximumIndex( j );
+      }
+
+      if( valid )
+        lut[ static_cast<std::size_t>(i) ] = std::make_pair( j, true );
+    }
+  }
+};
+
 } // namespace algorithms
 
 } // namespace persistentHomology
