4d8fe328dc7d32be2b8376758e2d5c83a1f74c37
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-22 13:24:49 +0100

    Finally using dualization for persistent intersection homology

--- tests/test_persistent_intersection_homology.cc
+++ tests/test_persistent_intersection_homology.cc
@@ -400,9 +400,9 @@ template <class T> void testDualization()
   ALEPH_ASSERT_EQUAL( numAllowableTwoChains, 1 );
 
   auto pairing1 = aleph::calculatePersistencePairing<aleph::persistentHomology::algorithms::Standard>( boundaryMatrix          , false, IndexType(s) );
-  auto pairing2 = aleph::calculatePersistencePairing<aleph::persistentHomology::algorithms::Standard>( boundaryMatrix.dualize(), false, IndexType(s) );
+  auto pairing2 = aleph::calculatePersistencePairing<aleph::persistentHomology::algorithms::Standard>( boundaryMatrix,           true , IndexType(s) );
   auto pairing3 = aleph::calculatePersistencePairing<aleph::persistentHomology::algorithms::Twist>   ( boundaryMatrix          , false, IndexType(s) );
-  auto pairing4 = aleph::calculatePersistencePairing<aleph::persistentHomology::algorithms::Twist>   ( boundaryMatrix.dualize(), false, IndexType(s) );
+  auto pairing4 = aleph::calculatePersistencePairing<aleph::persistentHomology::algorithms::Twist>   ( boundaryMatrix          , true , IndexType(s) );
 
   ALEPH_ASSERT_THROW( pairing1 == pairing2 );
   ALEPH_ASSERT_THROW( pairing3 == pairing4 );
@@ -486,7 +486,7 @@ template <class T> void testPinchedTorus()
   auto X1 = X0;
   auto X2 = K;
 
-  auto D2 = aleph::calculateIntersectionHomology( L, {X0,X1,X2}, aleph::PerversityGM( {0} ), false );
+  auto D2 = aleph::calculateIntersectionHomology( L, {X0,X1,X2}, aleph::PerversityGM( {0} ), true );
 
   ALEPH_ASSERT_EQUAL( D2.size(), 3 );
   ALEPH_ASSERT_EQUAL( D2[0].betti(), 1 );
@@ -596,7 +596,7 @@ template <class T> void testSphere()
   ALEPH_ASSERT_EQUAL( D2.front().dimension(), 0 );
   ALEPH_ASSERT_EQUAL( D2.front().betti(),     1 );
 
-  auto D3 = aleph::calculateIntersectionHomology( L, {X0,X0,K}, aleph::PerversityGM( {0} ), false );
+  auto D3 = aleph::calculateIntersectionHomology( L, {X0,X0,K}, aleph::PerversityGM( {0} ), true );
 
   ALEPH_ASSERT_EQUAL( D3.size(),         3 );
   ALEPH_ASSERT_EQUAL( D3[0].dimension(), 0 );
@@ -739,8 +739,8 @@ template <class T> void testTorus()
   auto X2 = X1;
   auto X3 = K;
 
-  auto D2 = aleph::calculateIntersectionHomology( L, {X0,X1,X2,X3}, aleph::PerversityGM( {0,0} ), false );
-  auto D3 = aleph::calculateIntersectionHomology( L, {X0,X1,X2,X3}, aleph::PerversityGM( {0,1} ), false );
+  auto D2 = aleph::calculateIntersectionHomology( L, {X0,X1,X2,X3}, aleph::PerversityGM( {0,0} ), true );
+  auto D3 = aleph::calculateIntersectionHomology( L, {X0,X1,X2,X3}, aleph::PerversityGM( {0,1} ), true );
 
   ALEPH_ASSERT_EQUAL( D2.size(), 4 );
   ALEPH_ASSERT_EQUAL( D3.size(), 4 );
@@ -791,9 +791,9 @@ template <class T> void testWedgeOfTwoCircles()
   SimplicialComplex Y0 = { {0}, {2} };
   SimplicialComplex Y1 = K;
 
-  auto D1 = aleph::calculateIntersectionHomology( K, {X0,X1}, aleph::Perversity( {-1} ), false );
-  auto D2 = aleph::calculateIntersectionHomology( K, {X0,X1}, aleph::Perversity( { 0} ), false );
-  auto D3 = aleph::calculateIntersectionHomology( K, {Y0,Y1}, aleph::Perversity( {-1} ), false );
+  auto D1 = aleph::calculateIntersectionHomology( K, {X0,X1}, aleph::Perversity( {-1} ) );
+  auto D2 = aleph::calculateIntersectionHomology( K, {X0,X1}, aleph::Perversity( { 0} ) );
+  auto D3 = aleph::calculateIntersectionHomology( K, {Y0,Y1}, aleph::Perversity( {-1} ) );
 
   ALEPH_ASSERT_EQUAL( D1.size(), 1 );
   ALEPH_ASSERT_EQUAL( D2.size(), 2 );
