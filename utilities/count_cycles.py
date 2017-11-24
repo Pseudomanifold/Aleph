c7d905580628405cd92dcf2b188cadb7be62d0a6
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-11-24 10:10:48 +0100

    Basic cycle counting script

---
+++ utilities/count_cycles.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# This file is part of 'Aleph - A Library for Exploring Persistent
+# Homology'. It counts elementary cycles in a graph and prints the
+# count to STDOUT. The script tries to be smart and uses *numbers*
+# detected in the input filenames in order to structure its output
+# accordingly.
+#
+# Original author: Bastian Rieck
+
+import networkx as nx
+
+import os
+import re
+import sys
+
+if __name__ == "__main__":
+  filenames = sys.argv[1:]
+
+  for filename in filenames:
+    G       = nx.read_gml(filename, label="id")
+    basis   = nx.cycle_basis(G)
+    matches = re.match(r'[^0-9]+(\d+).*', os.path.basename(filename))
+
+    if matches:
+      t = matches.group(1)
+      print("{}\t{}".format(t,len(basis)))
