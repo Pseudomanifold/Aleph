cd6e69517b9db42c1d0cc931971c2f1d795adc10
Bastian Rieck <bastian@rieck.ru>
2017-11-27 08:52:08 +0100

    Added loading routines for piecewise linear functions
    Next, attempting to obtain useful mean estimates and maybe some
    bootstrap calculations.

--- src/tools/piecewise_linear_function_smoothing.cc
+++ src/tools/piecewise_linear_function_smoothing.cc
@@ -1,19 +1,62 @@
 #include <aleph/math/Bootstrap.hh>
-
 #include <aleph/math/PiecewiseLinearFunction.hh>
 
+#include <aleph/utilities/String.hh>
+
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <getopt.h>
 
+using DataType = double;
+using Function = aleph::math::PiecewiseLinearFunction<DataType>;
+
+Function load( const std::string& filename )
+{
+  std::ifstream in( filename );
+  if( !in )
+    throw std::runtime_error( "Unable to open file for reading" );
+
+  std::string line;
+  std::vector< std::pair<DataType, DataType> > data;
+
+  while( std::getline( in, line ) )
+  {
+    line = aleph::utilities::trim( line );
+
+    if( line.empty() || line.front() == '#' )
+      continue;
+
+    DataType x = DataType();
+    DataType y = DataType();
+
+    std::istringstream converter( line );
+
+    converter >> x >> y;
+
+    if( !converter )
+      throw std::runtime_error( "Conversion failed" );
+
+    data.push_back( std::make_pair(x,y) );
+  }
+
+  return Function( data.begin(), data.end() );
+}
+
 int main( int argc, char** argv )
 {
-  double alpha = 0.95;
+  DataType alpha               = 0.95;
+  unsigned numBootstrapSamples = 10;
 
   {
     static option commandLineOptions[] =
     {
       { "alpha", required_argument, nullptr, 'a' },
+      { "n"    , required_argument, nullptr, 'n' },
       { nullptr, 0                , nullptr,  0  }
     };
 
@@ -23,9 +66,24 @@ int main( int argc, char** argv )
       switch( option )
       {
       case 'a':
-        alpha = std::stod( optarg );
+        alpha = DataType( std::stod( optarg ) );
+        break;
+      case 'n':
+        numBootstrapSamples = unsigned( std::stoull( optarg ) );
+        break;
+      default:
         break;
       }
     }
   }
+
+  if( argc - optind <= 1 )
+    return -1;
+
+  // Load functions ----------------------------------------------------
+
+  std::vector<Function> functions;
+
+  for( int i = optind; i < argc; i++ )
+    functions.push_back( load( argv[i] ) );
 }
