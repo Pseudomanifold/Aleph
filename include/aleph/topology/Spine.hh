bf02ca6a291af721c40493e5b28f4a06ecf225a9
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-12-08 18:04:34 +0100

    Fixed addition of empty cofaces
    It does not make sense to *overwrite* cofaces that are already stored in
    the complex; this issue was caught by a unit test.

--- include/aleph/topology/Spine.hh
+++ include/aleph/topology/Spine.hh
@@ -33,7 +33,8 @@ template <class SimplicialComplex> CofaceMap<typename SimplicialComplex::ValueTy
     // Adding an *empty* list of cofaces (so far) for this simplex
     // simplifies the rest of the code because there is no need to
     // check for the existence of a simplex.
-    cofaces[s] = {};
+    if( cofaces.find(s) == cofaces.end() )
+      cofaces[s] = {};
 
     for( auto itFace = s.begin_boundary(); itFace != s.end_boundary(); ++itFace )
       cofaces[ *itFace ].insert( s );
