d4f1993efe06557a87ab40a7137449a9959c152d
Bastian Rieck <bastian.rieck@iwr.uni-heidelberg.de>
2017-12-20 17:11:15 +0100

    Extended mean--shift smoothing algorithm

--- include/aleph/containers/MeanShift.hh
+++ include/aleph/containers/MeanShift.hh
@@ -17,13 +17,56 @@ template <
 > void meanShiftSmoothing(
   const Container& container,
   unsigned k,
+  unsigned n = 1,
   InputIterator begin, InputIterator end )
 {
   using T = typename std::iterator_traits<InputIterator>::value_type;
   auto n  = container.size();
 
-  // Makes it easier to permit random access to the container.
+  // Makes it easier to permit random access to the container; I am
+  // assuming that the indices correspond to each other.
   std::vector<T> data( begin, end );
+
+  Wrapper nearestNeighbours( container );
+
+  using IndexType   = typename Wrapper::IndexType;
+  using ElementType = typename Wrapper::ElementType;
+
+  std::vector< std::vector<IndexType> > indices;
+  std::vector< std::vector<ElementType> > distances;
+
+  nearestNeighbours.neighbourSearch( k+1, indices, distances );
+
+  for( unsigned iteration = 0; iteration < n; iteration++ )
+  {
+    std::vector<T> data_( data.size() );
+
+    for( std::size_t i = 0; i < n; i++ )
+    {
+      auto&& neighbours_  = indices[i];
+      auto&& distances_   = distances[i];
+
+      // TODO: make configurable; it might also make sense to permit the
+      // usage of arbitrary functors here
+      aleph::math::KahanSummation<T> value        = 0.0;
+      aleph::math::KahanSummation<T> sumOfWeights = 0.0;
+
+      for( std::size_t j = 0; j < neighbours_.size(); j++ )
+      {
+        auto index    = neighbours_[j];
+        auto weight   = distances_[j] > 0 ? 1.0 / ( distances_[j] * distances_[j] ) : 1.0;
+        value        += result[ index ] * weight;
+        sumOfWeights += weight;
+      }
+
+      value    /= sumOfWeights;
+      data_[i]  = value;
+    }
+
+    data.swap( data_ );
+  }
+
+  return data;
 }
 
 } // namespace containers
