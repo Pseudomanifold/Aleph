71b1357e9b3940b691c98750612e759047eb31e0
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-02-14 14:15:47 +0100

    Added test for GraphML weight parsing
    I am now confident that GraphML parsing works as intended, at least for
    these simple test files.
    
    This fixes issue #35.

--- tests/test_io_graphml.cc
+++ tests/test_io_graphml.cc
@@ -30,6 +30,28 @@ template <class D, class V> void test( const std::string& filename )
   ALEPH_ASSERT_EQUAL( numNodes, 6 );
   ALEPH_ASSERT_EQUAL( numEdges, 7 );
 
+  std::vector<D> weights;
+  weights.reserve( K.size() );
+
+  std::transform( K.begin(), K.end(), std::back_inserter( weights ),
+    [] ( const Simplex& s )
+    {
+      return s.data();
+    }
+  );
+
+  auto n00 = static_cast<std::size_t>( std::count( weights.begin(), weights.end(), D(0.0) ) );
+  auto n10 = static_cast<std::size_t>( std::count( weights.begin(), weights.end(), D(1.0) ) );
+  auto n11 = static_cast<std::size_t>( std::count( weights.begin(), weights.end(), D(1.1) ) );
+  auto n20 = static_cast<std::size_t>( std::count( weights.begin(), weights.end(), D(2.0) ) );
+
+  ALEPH_ASSERT_EQUAL( K.size(), weights.size() );
+  ALEPH_ASSERT_EQUAL( n00+n10+n11+n20, weights.size() );
+
+  ALEPH_ASSERT_EQUAL( n10, 2 );
+  ALEPH_ASSERT_EQUAL( n11, 1 );
+  ALEPH_ASSERT_EQUAL( n20, 1 );
+
   ALEPH_TEST_END();
 }
 
