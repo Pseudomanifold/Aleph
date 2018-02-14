2b194c8a414cfef94feecbe1cf4636d9686eea7c
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-02-14 17:14:00 +0100

    Skeleton for function analysis tool
    I am currently unsure about the existence of simple conversion
    code for these value sequences into simplicial complexes. I am
    not recalling whether I actually worked on this or not.

---
+++ src/tools/function_analysis.cc
@@ -0,0 +1,57 @@
+#include <aleph/topology/Simplex.hh>
+#include <aleph/topology/SimplicialComplex.hh>
+
+#include <aleph/utilities/String.hh>
+
+#include <fstream>
+#include <istream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using DataType          = double;
+using VertexType        = unsigned;
+using Simplex           = aleph::topology::Simplex<DataType, VertexType>;
+using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
+
+std::vector<SimplicialComplex> readData( std::istream& in )
+{
+  std::string line;
+
+  while( std::getline( in, line ) )
+  {
+    auto tokens
+      = aleph::utilities::split(
+          line,
+          std::string( "[:;,[:space:]]+" )
+    );
+
+    std::vector<DataType> values;
+    values.reserve( tokens.size() );
+
+    for( auto&& token : tokens )
+    {
+      bool success = false;
+      auto value   = aleph::utilities::convert<DataType>( token, success );
+
+      if( !success )
+        throw std::runtime_error( "Unable to convert token to expected data type" );
+
+      values.emplace_back( value );
+    }
+  }
+}
+
+void usage()
+{
+}
+
+int main( int argc, char** argv )
+{
+  if( argc < 1 )
+  {
+    usage();
+    return -1;
+  }
+}
