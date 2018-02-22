9d7dd144368a3f533c49e0bc4bb8330ea7822204
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-02-22 13:51:14 +0100

    Basic test case for correlation dimension

---
+++ tests/test_fractal_dimension.cc
@@ -0,0 +1,33 @@
+#include <aleph/containers/FractalDimension.hh>
+#include <aleph/containers/PointCloud.hh>
+
+#include <aleph/geometry/distances/Euclidean.hh>
+
+#include <tests/Base.hh>
+
+using namespace aleph::geometry::distances;
+using namespace aleph::containers;
+using namespace aleph;
+
+template <class T> void testCorrelationDimension()
+{
+  ALEPH_TEST_BEGIN( "Correlation dimension" );
+
+  using PointCloud = PointCloud<T>;
+  PointCloud pc = load<T>(
+    CMAKE_SOURCE_DIR + std::string( "/tests/input/Iris_comma_separated.txt" )
+  );
+
+  auto cds = correlationDimensionIntegral( pc, Euclidean<double>() );
+
+  ALEPH_ASSERT_THROW( cds.x.empty() == false );
+  ALEPH_ASSERT_THROW( cds.y.empty() == false );
+
+  ALEPH_TEST_END();
+}
+
+int main(int, char**)
+{
+  testCorrelationDimension<float> ();
+  testCorrelationDimension<double>();
+}
