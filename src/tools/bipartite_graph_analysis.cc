e38d438db577c3102d694d66599feb94869da6cc
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-02-23 15:23:04 +0100

    Added basic normalization strategy

--- src/tools/bipartite_graph_analysis.cc
+++ src/tools/bipartite_graph_analysis.cc
@@ -7,25 +7,55 @@
 #include <aleph/topology/SimplicialComplex.hh>
 #include <aleph/topology/io/BipartiteAdjacencyMatrix.hh>
 
+#include <algorithm>
 #include <iostream>
+#include <limits>
 #include <vector>
+
+#include <getopt.h>
 
 int main( int argc, char** argv )
 {
+  bool normalize = false;
+
+  {
+    static option commandLineOptions[] =
+    {
+      { "normalize", no_argument, nullptr, 'n' },
+      { nullptr    , 0          , nullptr,  0  }
+    };
+
+    int option = 0;
+    while( ( option = getopt_long( argc, argv, "n", commandLineOptions, nullptr ) ) != -1 )
+    {
+      switch( option )
+      {
+      case 'n':
+        normalize = true;
+        break;
+      default:
+        break;
+      }
+    }
+  }
+
   using DataType          = double;
   using VertexType        = unsigned short;
   using Simplex           = aleph::topology::Simplex<DataType, VertexType>;
   using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
 
+  DataType minData = std::numeric_limits<DataType>::max();
+  DataType maxData = std::numeric_limits<DataType>::lowest();
+
   // 1. Read simplicial complexes --------------------------------------
 
   std::vector<SimplicialComplex> simplicialComplexes;
-  simplicialComplexes.reserve( static_cast<unsigned>( argc - 1 ) );
+  simplicialComplexes.reserve( static_cast<unsigned>( argc - optind - 1 ) );
 
   {
     aleph::topology::io::BipartiteAdjacencyMatrixReader reader;
 
-    for( int i = 1; i < argc; i++ )
+    for( int i = optind; i < argc; i++ )
     {
       auto filename = std::string( argv[i] );
 
@@ -35,6 +65,15 @@ int main( int argc, char** argv )
       reader( filename, K );
 
       std::cerr << "finished\n";
+
+      // *Always* determine minimum and maximum weights so that we may
+      // report them later on. They are only used for normalization in
+      // the persistence diagram calculation step.
+      for( auto&& s : K )
+      {
+        minData = std::min( minData, s.data() );
+        maxData = std::max( maxData, s.data() );
+      }
 
       simplicialComplexes.emplace_back( K );
     }
@@ -49,6 +88,25 @@ int main( int argc, char** argv )
     auto&& D      = diagrams.front();
 
     D.removeUnpaired();
+
+    if( normalize )
+    {
+      using PersistenceDiagram = aleph::PersistenceDiagram<DataType>;
+      using Point              = typename PersistenceDiagram::Point;
+
+      // Ensures that points are in [-1:+1]
+      std::transform( D.begin(), D.end(), D.begin(),
+        [&minData, &maxData] ( const Point& p )
+        {
+          auto x = p.x();
+          auto y = p.y();
+          x      = 2 * (x - minData) / (maxData - minData) - 1;
+          y      = 2 * (y - minData) / (maxData - minData) - 1;
+
+          return Point( x,y );
+        }
+      );
+    }
 
     std::cout << i << "\t" << aleph::pNorm( D ) << "\n";
   }
