ec09d1fdfb67cca050c9decd6fb408eb8d75b73a
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-02-23 12:14:32 +0100

    Added persistent homology calculation and $p$-norm summary statistic

--- src/tools/bipartite_graph_analysis.cc
+++ src/tools/bipartite_graph_analysis.cc
@@ -1,3 +1,8 @@
+#include <aleph/persistenceDiagrams/Norms.hh>
+#include <aleph/persistenceDiagrams/PersistenceDiagram.hh>
+
+#include <aleph/persistentHomology/Calculation.hh>
+
 #include <aleph/topology/Simplex.hh>
 #include <aleph/topology/SimplicialComplex.hh>
 #include <aleph/topology/io/BipartiteAdjacencyMatrix.hh>
@@ -12,6 +17,8 @@ int main( int argc, char** argv )
   using Simplex           = aleph::topology::Simplex<DataType, VertexType>;
   using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
 
+  // 1. Read simplicial complexes --------------------------------------
+
   std::vector<SimplicialComplex> simplicialComplexes;
   simplicialComplexes.reserve( static_cast<unsigned>( argc - 1 ) );
 
@@ -20,7 +27,7 @@ int main( int argc, char** argv )
 
     for( int i = 1; i < argc; i++ )
     {
-      auto filename = argv[i];
+      auto filename = std::string( argv[i] );
 
       std::cerr << "* Processing " << filename << "...";
 
@@ -31,5 +38,18 @@ int main( int argc, char** argv )
 
       simplicialComplexes.emplace_back( K );
     }
+  }
+
+  // 2. Calculate persistent homology ----------------------------------
+
+  for( std::size_t i = 0; i < simplicialComplexes.size(); i++ )
+  {
+    auto&& K      = simplicialComplexes[i];
+    auto diagrams = aleph::calculatePersistenceDiagrams( K );
+    auto&& D      = diagrams.front();
+
+    D.removeUnpaired();
+
+    std::cout << i << "\t" << aleph::pNorm( D ) << "\n";
   }
 }
