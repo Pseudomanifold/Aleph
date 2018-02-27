9eb84b4043f902f3b767ac2110c27c0807aeb151
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-02-27 15:25:01 +0100

    Simplified matrix output
    Else, it is harder to process the matrices in other applications. Since
    I am only using this as a debugging aid, it makes no sense to make this
    too complex.

--- include/aleph/math/SymmetricMatrix.hh
+++ include/aleph/math/SymmetricMatrix.hh
@@ -2,8 +2,7 @@
 #define ALEPH_MATH_SYMMETRIC_MATRIX_HH__
 
 #include <algorithm>
-#include <iomanip>
-#include <iosfwd>
+#include <ostream>
 #include <stdexcept>
 
 #include <cstddef>
@@ -172,26 +171,21 @@ private:
 
 template <class T> std::ostream& operator<<( std::ostream& o, const aleph::math::SymmetricMatrix<T>& M )
 {
-  auto flags( o.flags() );
-
   auto n = M.numRows();
-
-  o << std::setfill( ' ' );
 
   for( decltype(n) i = 0; i < n; i++ )
   {
     for( decltype(n) j = 0; j < n; j++ )
     {
       if( j != 0 )
-        o << ", ";
+        o << " ";
 
-      o << std::setw( 10 ) << std::right << M(i,j);
+      o << M(i,j);
     }
 
     o << "\n";
   }
 
-  o.flags( flags );
   return o;
 }
 
