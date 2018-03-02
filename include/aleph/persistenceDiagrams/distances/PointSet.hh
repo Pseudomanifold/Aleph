47b7a503cfb9f68049d46575efc0c01e3220e0a1
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-03-02 17:47:32 +0100

    Started preparing header for point set distance calculations for persistence diagrams

---
+++ include/aleph/persistenceDiagrams/distances/PointSet.hh
@@ -0,0 +1,29 @@
+#ifndef ALEPH_PERSISTENCE_DIAGRAMS_DISTANCES_POINT_SET_HH__
+#define ALEPH_PERSISTENCE_DIAGRAMS_DISTANCES_POINT_SET_HH__
+
+/**
+  @file  PointSet.hh
+  @brief Distance measures based on point sets for persistence diagrams
+
+  The purpose of this file is to collect some distance measures that
+  treat a persistence diagram as a simple point set. Hence, parts of
+  the topological structure will *not* be considered on purpose. The
+  functions based in this file are mainly based on one publication:
+
+  > Distance Measures for Point Sets and Their Computation
+  > Thomas Eiter and Heikki Mannila
+  > Acta Informatica, Volume 34, Issue 2, pp. 109–133
+
+  @see https://doi.org/10.1007/s002360050075
+*/
+
+namespace aleph
+{
+
+namespace distances
+{
+} // namespace distances
+
+} // namespace aleph
+
+#endif
