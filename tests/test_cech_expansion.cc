96abad204f36ef28b701ca252f84f22c62e94164
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-03-09 16:31:04 +0100

    Extended test

--- tests/test_cech_expansion.cc
+++ tests/test_cech_expansion.cc
@@ -1,5 +1,7 @@
 #include <tests/Base.hh>
 
+#include <aleph/containers/PointCloud.hh>
+
 #include <aleph/geometry/CechComplex.hh>
 
 #include <aleph/topology/Simplex.hh>
@@ -14,21 +16,26 @@
 
 #include <cmath>
 
+using namespace aleph::containers;
 using namespace aleph::geometry;
 using namespace aleph::topology;
 using namespace aleph;
 
-template <class Data, class Vertex> void triangle()
+template <class T> void triangle()
 {
   ALEPH_TEST_BEGIN( "Triangle" );
+
+  using PointCloud = PointCloud<T>;
+  PointCloud pc    = load<T>( CMAKE_SOURCE_DIR + std::string( "/tests/input/Triangle_point_cloud.txt" ) );
+
+  ALEPH_ASSERT_EQUAL( pc.dimension(), 2);
+  ALEPH_ASSERT_EQUAL( pc.size()     , 3);
 
   ALEPH_TEST_END();
 }
 
 int main()
 {
-  triangle<double, unsigned>();
-  triangle<double, short   >();
-  triangle<float,  unsigned>();
-  triangle<float,  short   >();
+  triangle<double>();
+  triangle<float> ();
 }
