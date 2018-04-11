9c89a3cfde2620371aa83a763abd2e5d3a4d0964
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-04-11 18:42:04 +0200

    Extended stratified graph creation script
    * Added support for non-standard filtration
    * Added total persistence calculation

--- src/tools/random_stratified_graphs.cc
+++ src/tools/random_stratified_graphs.cc
@@ -1,5 +1,10 @@
 #include <aleph/persistentHomology/Calculation.hh>
 
+#include <aleph/persistenceDiagrams/Norms.hh>
+#include <aleph/persistenceDiagrams/PersistenceDiagram.hh>
+
+#include <aleph/topology/filtrations/Data.hh>
+
 #include <aleph/topology/Simplex.hh>
 #include <aleph/topology/SimplicialComplex.hh>
 
@@ -17,6 +22,100 @@ using VertexType        = unsigned short;
 using Simplex           = aleph::topology::Simplex<DataType, VertexType>;
 using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
 
+// FIXME: copied from bipartite graph analysis tool
+SimplicialComplex makeFiltration( const SimplicialComplex& K, bool upper = false )
+{
+  std::vector<Simplex> simplices;
+  simplices.reserve( K.size() );
+
+  // Keep vertices if they are above/below the desired data type
+  // threshold for the filtration.
+  std::transform( K.begin(), K.end(), std::back_inserter( simplices ),
+    [&upper] ( const Simplex& s )
+    {
+      if( ( upper && s.data() > DataType() ) || ( !upper && s.data() < DataType() ) )
+        return s;
+
+      // Copy the simplex but set its weight to be zero because it does
+      // not correspond to any structure that we want to learn.
+      else
+      {
+        std::vector<VertexType> vertices( s.begin(), s.end() );
+        return Simplex( s.begin(), s.end(), DataType() );
+      }
+    }
+  );
+
+  // Ensure that all vertices are created at threshold zero. This
+  // indicates that vertices are always available in the network,
+  // regardless of weight threshold.
+  //
+  // FIXME: this somewhat interferes with the weight selection in
+  // the reader class; not sure how to merge those aspects
+  std::transform( simplices.begin(), simplices.end(), simplices.begin(),
+    [] ( const Simplex& s )
+    {
+      if( s.dimension() == 0 )
+        return Simplex( *s.begin(), DataType() );
+      else
+        return s;
+    }
+  );
+
+  // Remove higher-dimensional simplices (edges) that do not have
+  // a part in the current filtration.
+  simplices.erase(
+    std::remove_if( simplices.begin(), simplices.end(),
+      [] ( const Simplex& s )
+      {
+        return s.dimension() != 0 && s.data() == DataType();
+      }
+    ), simplices.end()
+  );
+
+  return SimplicialComplex( simplices.begin(), simplices.end() );
+}
+
+// FIXME: copied from bipartite graph analysis tool
+SimplicialComplex makeLowerFiltration( const SimplicialComplex& K )
+{
+  auto L = makeFiltration( K );
+  L.sort(
+    aleph::topology::filtrations::Data<Simplex, std::greater<DataType> >()
+  );
+
+  return L;
+}
+
+// FIXME: copied from bipartite graph analysis tool
+SimplicialComplex makeUpperFiltration( const SimplicialComplex& K )
+{
+  auto L = makeFiltration( K, true );
+  L.sort(
+    aleph::topology::filtrations::Data<Simplex, std::less<DataType> >()
+  );
+
+  return makeFiltration( K, true );
+}
+
+using PersistenceDiagram = aleph::PersistenceDiagram<DataType>;
+using Point              = typename PersistenceDiagram::Point;
+
+// FIXME: copied from bipartite graph analysis tool
+PersistenceDiagram merge( const PersistenceDiagram& D, const PersistenceDiagram& E )
+{
+  PersistenceDiagram F;
+
+  if( D.dimension() != F.dimension() )
+    throw std::runtime_error( "Persistence diagram dimensions have to agree" );
+
+  for( auto&& diagram : { D, E } )
+    for( auto&& p : diagram )
+      F.add( p.x(), p.y() );
+
+  return F;
+}
+
 template
 <
   class Engine,       // random engine to use for weight generation (e.g. std::default_random_engine)
@@ -28,7 +127,6 @@ SimplicialComplex makeRandomStratifiedGraph(
   Distribution& distribution
 )
 {
-
   auto n = strata.size();
 
   if( n <= 1 )
@@ -99,10 +197,34 @@ int main( int, char** )
   );
 
   auto K
-    = makeRandomStratifiedGraph( {2,3,1},
+    = makeRandomStratifiedGraph( {2,3}, // FIXME: {2,3,1} for the complete network
                                  engine,
                                  distribution
   );
 
-  std::cerr << K << "\n";
+  PersistenceDiagram D;
+
+  // This uses the upper--lower filtration, which is not theoretically
+  // justified. This should be configurable.
+  {
+    auto L = makeLowerFiltration( K );
+    auto U = makeUpperFiltration( K );
+
+    auto lowerDiagrams = aleph::calculatePersistenceDiagrams( L );
+    auto upperDiagrams = aleph::calculatePersistenceDiagrams( U );
+
+    if( !lowerDiagrams.empty() && !upperDiagrams.empty() )
+    {
+      D = merge(
+        lowerDiagrams.front(),
+        upperDiagrams.front()
+      );
+    }
+  }
+
+  D.removeDiagonal();
+  D.removeUnpaired();
+
+  if( !D.empty() )
+    std::cout << aleph::pNorm( D ) << "\n";
 }
