8baa1006d20a211677882e6ace7eebd26c68192c
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-04-12 13:39:21 +0200

    Added `.so` file for bindings setup

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -3,8 +3,11 @@ from distutils.core import setup
 setup(
   name        = 'Aleph',
   version     = '${PACKAGE_VERSION}', # TODO: might want to use commit ID here
-  package_dir = {
-      '': '${CMAKE_CURRENT_BINARY_DIR}'
-  },
   packages    = [ 'aleph' ]
+  package_dir = {
+    '': '${CMAKE_CURRENT_BINARY_DIR}'
+  },
+  package_data = {
+    '': ['aleph.so']
+  }
 )
