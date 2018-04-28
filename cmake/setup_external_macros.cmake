c277b03538c2a952d145de32f1245aaa3660cf1b
Bastian Rieck <bastian@rieck.ru>
2018-04-28 13:57:58 +0200

    Setup for external macros

---
+++ cmake/setup_external_macros.cmake
@@ -0,0 +1,8 @@
+# 
+# Includes all external CMake macros that we are going to use for
+# building Aleph
+#
+
+INCLUDE( CheckCXXCompilerFlag )
+INCLUDE( CheckCXXSourceCompiles )
+INCLUDE( CheckCXXSourceRuns )
