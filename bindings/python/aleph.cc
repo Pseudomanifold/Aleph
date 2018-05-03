df53267a2bbaa61a52af2a6b8dbc92e4ca8bca35
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-05-03 13:52:32 +0200

    Extended `__array__` conversion for persistence pairings

--- bindings/python/aleph.cc
+++ bindings/python/aleph.cc
@@ -283,7 +283,7 @@ void wrapPersistenceDiagram( py::module& m )
     .def_property( "dimension", &PersistenceDiagram::setDimension, &PersistenceDiagram::dimension )
     .def_property_readonly( "betti", &PersistenceDiagram::betti )
     .def( "__array__",
-      [] (PersistenceDiagram &D)
+      [] ( PersistenceDiagram& D )
       {
         auto n_points    = D.size();
         DataType* buffer = new DataType[ 2*n_points ];
@@ -359,49 +359,37 @@ void wrapPersistencePairing( py::module& m )
 
         return stream.str();
       }
-    );
-
-    //.def( "__array__",
-    //  [] (PersistenceDiagram &D) {
-    //  auto n_points = D.size();
-    //  DataType *buffer = new DataType[2*n_points];
-
-    //  for (auto it = D.begin(); it != D.end(); it++) {
-    //    auto index = std::distance(D.begin(), it);
-    //    buffer[2*index] = it->x();
-    //    buffer[2*index+1] = it->y();
-    //  }
-    //  // Based on https://stackoverflow.com/questions/44659924/returning-numpy-arrays-via-pybind11
-    //  // Callback that allows buffer to be freed if not used in python anymore
-    //  py::capsule free_when_done(buffer, [](void *f) {
-    //        DataType *buf = reinterpret_cast<DataType *>(f);
-    //        delete[] buf;
-    //    });
-
-    //  return py::array_t<DataType>(
-    //    {static_cast<unsigned long>(n_points), static_cast<unsigned long>(2)}, // shape
-    //    {static_cast<unsigned long>(2*sizeof(DataType)), static_cast<unsigned long>(sizeof(DataType))}, // Stride
-    //    buffer, // the data pointer
-    //    free_when_done); // numpy array references this parent
-    //});
-
-  using Point = typename PersistenceDiagram::Point;
-
-  py::class_<Point>(m, "PersistenceDiagram.Point" )
-    .def( py::init<DataType>() )
-    .def( py::init<DataType, DataType>() )
-    .def( "__eq__", &Point::operator== )
-    .def( "__ne__", &Point::operator!= )
-    .def( "__repr__",
-      [] ( const Point& p )
-      {
-        return "<" + std::to_string( p.x() ) + "," + std::to_string( p.y() ) + ">";
-      }
     )
-    .def_property_readonly( "x", &Point::x )
-    .def_property_readonly( "y", &Point::y )
-    .def_property_readonly( "persistence", &Point::persistence )
-    .def_property_readonly( "unpaired"   , &Point::isUnpaired );
+    .def( "__array__",
+      [] ( PersistencePairing& pairing )
+      {
+        auto n_points    = pairing.size();
+        DataType* buffer = new DataType[ 2*n_points ];
+
+        for( auto it = pairing.begin(); it != pairing.end(); ++it )
+        {
+          auto index        = std::distance( pairing.begin(), it );
+          buffer[2*index  ] = it->first;
+          buffer[2*index+1] = it->second;
+        }
+
+        // Based on https://stackoverflow.com/questions/44659924/returning-numpy-arrays-via-pybind11
+        // Callback that allows buffer to be freed if not used in python anymore
+        py::capsule free_when_done( buffer,
+          [] (void* f)
+          {
+            DataType* buf = reinterpret_cast<DataType*>( f );
+            delete[] buf;
+          }
+        );
+
+        return py::array_t<DataType>(
+          {static_cast<unsigned long>(n_points), static_cast<unsigned long>(2)},                          // shape
+          {static_cast<unsigned long>(2*sizeof(DataType)), static_cast<unsigned long>(sizeof(DataType))}, // stride
+          buffer,                                                                                         // the data pointer
+          free_when_done);                                                                                // numpy array references this parent
+      }
+    );
 }
 
 void wrapPersistentHomologyCalculation( py::module& m )
