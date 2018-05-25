276ff3f96a304b4d58e76de0cfa5b5e6fc03c586
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-05-25 09:12:59 +0200

    Made bootstrap samples configurable
    This addresses issue #40 and even goes beyond it by making samples and
    the alpha parameter configurable.

--- src/tools/persistence_indicator_function_confidence_sets.cc
+++ src/tools/persistence_indicator_function_confidence_sets.cc
@@ -13,6 +13,8 @@
 
 #include <cmath>
 
+#include <getopt.h>
+
 using DataType                     = double;
 using PersistenceDiagram           = aleph::PersistenceDiagram<DataType>;
 using PersistenceIndicatorFunction = decltype( aleph::persistenceIndicatorFunction( PersistenceDiagram() ) );
@@ -37,6 +39,41 @@ unsigned index( unsigned int samples, double alpha )
 
 int main( int argc, char** argv )
 {
+
+  auto alpha                   = 0.05;
+  unsigned numBootstrapSamples = 50;
+
+  {
+    static option commandLineOptions[] =
+    {
+      { "alpha"    , required_argument, nullptr, 'a' },
+      { "bootstrap", required_argument, nullptr, 'b' },
+      { nullptr    , 0                , nullptr,  0  }
+    };
+
+    int c = 0;
+    while( ( c = getopt_long( argc, argv, "a:b:", commandLineOptions, nullptr ) ) != -1 )
+    {
+      switch( c )
+      {
+      case 'a':
+        alpha = std::stod( optarg );
+        break;
+
+      case 'b':
+        numBootstrapSamples = static_cast<unsigned>( std::stoul( optarg ) );
+        break;
+
+      // TODO: throw exception
+      default:
+        break;
+      }
+    }
+
+    // TODO: check alpha
+    // TODO: check bootstrap samples
+  }
+
   std::vector<PersistenceIndicatorFunction> persistenceIndicatorFunctions;
   persistenceIndicatorFunctions.reserve( static_cast<std::size_t>( argc - 1 ) );
 
@@ -53,9 +90,6 @@ int main( int argc, char** argv )
 
     std::cerr << "finished\n";
   }
-
-  // TODO: make configurable
-  unsigned numBootstrapSamples = 50;
 
   std::vector<PersistenceIndicatorFunction> meanReplicates;
   meanReplicates.reserve( numBootstrapSamples );
@@ -83,7 +117,6 @@ int main( int argc, char** argv )
 
   std::sort( theta.begin(), theta.end() );
 
-  auto alpha    = 0.05;
   auto quantile = theta.at( index( numBootstrapSamples, alpha / 2 ) );
   auto fLower   = empiricalMean - quantile / std::sqrt( persistenceIndicatorFunctions.size() );
   auto fUpper   = empiricalMean + quantile / std::sqrt( persistenceIndicatorFunctions.size() );
