088b1fac5212227ca21b293fb2d88ccdd6d6da88
Bastian Rieck <bastian@rieck.ru>
2018-05-26 19:24:16 +0200

    Removed obsolete code for Python bindings

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -69,37 +69,4 @@ setup(
     ext_modules = [SourceExtension('aleph')],
     cmdclass    = dict(build_ext=Build),
     zip_safe    = False
-
-    #package_data = {
-    #  '': ['aleph.so']
-    #}
-    #name='cmake_example',
-    #version='0.0.1',
-    #author='Dean Moldovan',
-    #author_email='dean0x7d@gmail.com',
-    #description='A test project using pybind11 and CMake',
-    #long_description='',
-    #ext_modules=[CMakeExtension('cmake_example')],
-    #cmdclass=dict(build_ext=CMakeBuild),
-    #zip_safe=False,
 )
-
-
-
-#from distutils.core import setup
-#
-#import sys
-#if sys.version_info < (3,0):
-#  sys.exit('Sorry, Python < 3.0 is not supported')
-#
-#setup(
-#  name        = 'Aleph',
-#  version     = '${PACKAGE_VERSION}', # TODO: might want to use commit ID here
-#  packages    = [ 'aleph' ],
-#  package_dir = {
-#    '': '${CMAKE_CURRENT_BINARY_DIR}'
-#  },
-#  package_data = {
-#    '': ['aleph.so']
-#  }
-#)
