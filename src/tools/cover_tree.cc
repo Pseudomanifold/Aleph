a5cc1bbacc1f4d760ba27a9887ccc10ef587dd18
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-05-30 14:48:08 +0200

    Basic point reader function for cover tree tool
    It might make sense to offload this function into another file, but for
    now we do not actually have to load points properly---except for tests,
    where we can use a more "hacky" way of doing so.

--- src/tools/cover_tree.cc
+++ src/tools/cover_tree.cc
@@ -8,8 +8,64 @@
 */
 
 #include <aleph/geometry/CoverTree.hh>
+#include <aleph/geometry/Point.hh>
+
+#include <aleph/utilities/String.hh>
+
+#include <algorithm>
+#include <fstream>
+#include <istream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include <getopt.h>
+
+using DataType = double;
+using Point    = aleph::geometry::Point<DataType>;
+
+std::vector<Point> load( std::istream& in )
+{
+  using namespace aleph::utilities;
+
+  std::vector<Point> points;
+  std::string line;
+
+  while( std::getline( in, line ) )
+  {
+    line        = trim( line );
+    auto tokens = splitByWhitespace( line );
+
+    // Skip empty lines and comment lines
+    if( line.empty() || line.front() == '#' || tokens.empty() )
+      continue;
+
+    std::vector<DataType> values;
+
+    std::transform( tokens.begin(), tokens.end(),
+      std::back_inserter( values ),
+      [] ( const std::string& token )
+      {
+        return static_cast<DataType>( std::stod( token ) );
+      }
+    );
+
+    points.emplace_back( Point( values.begin(), values.end() ) );
+  }
+
+  return points;
+}
+
+std::vector<Point> load( const std::string& filename )
+{
+  std::ifstream in( filename );
+  if( !in )
+    throw std::runtime_error( "Unable to open input filename" );
+
+  return load( in );
+}
+
+
 
 int main( int, char** )
 {
