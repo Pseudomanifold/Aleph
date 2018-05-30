7266ac247469e51af9bc186eaaa177723226afeb
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-05-30 12:52:34 +0200

    Skeleton for cover tree creation

---
+++ src/tools/cover_tree.cc
@@ -0,0 +1,16 @@
+/*
+  This is a tool shipped by 'Aleph - A Library for Exploring Persistent
+  Homology'.
+
+  Its purpose is to create a *cover tree* for an input point cloud
+  of arbitrary dimensionality. The cover tree creation process can
+  be manipulated to some extent.
+*/
+
+#include <aleph/geometry/CoverTree.hh>
+
+#include <getopt.h>
+
+int main( int, char** )
+{
+}
