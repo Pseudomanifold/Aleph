e066546ff3cd7a27da8e03151b1e7684cb099324
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-03-20 15:06:44 +0100

    Skeleton for adjacency matrix conversion script

---
+++ utilities/numpy_to_adjacency_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+# numpy_to_adjacency_matrix.py: converts an `.npy` file to a (textual!)
+# adjacency matrix.
+
+
+import argparse
+import numpy as np
+
+
+if __name__ == '__main__':
+    pass
