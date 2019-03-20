31098eae069f39a8a4ce30e291324ea4f6747846
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-03-20 18:07:13 +0100

    Added proper adjacency matrix conversion to utility script

--- utilities/numpy_to_adjacency_matrix.py
+++ utilities/numpy_to_adjacency_matrix.py
@@ -5,8 +5,27 @@
 
 
 import argparse
+import sys
+
 import numpy as np
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('INPUT', type=str, help='Input file')
+    parser.add_argument('-o', '--output',
+        type=str,
+        help='Output file (optional)'
+    )
+
+    args = parser.parse_args()
+
+    M = np.load(args.INPUT)
+
+    if not args.output or args.output == '-':
+        output = sys.stdout
+    else:
+        output = args.output
+
+    # TODO: make output format configurable?
+    np.savetxt(output, M)
