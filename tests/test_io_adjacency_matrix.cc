b255e3a5e61749d9784d9f3a2d72796d4a339f58
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-03-20 18:37:08 +0100

    Added simple adjacency matrix loading test

---
+++ tests/test_io_adjacency_matrix.cc
@@ -0,0 +1,83 @@
+#include <aleph/topology/Simplex.hh>
+#include <aleph/topology/SimplicialComplex.hh>
+
+#include <aleph/topology/io/AdjacencyMatrix.hh>
+
+#include <tests/Base.hh>
+
+#include <sstream>
+
+auto inputSimple =
+  "0 1 3\n"
+  "1 7 5\n"
+  "3 5 9\n";
+
+auto inputBroken =
+  "0 1 2\n"
+  "1 4\n";
+
+template <class T> void testSimple()
+{
+  ALEPH_TEST_BEGIN( "Simple adjacency matrices" );
+
+  std::stringstream stream;
+
+  using DataType          = T;
+  using Simplex           = aleph::topology::Simplex<DataType>;
+  using SimplicialComplex = aleph::topology::SimplicialComplex<Simplex>;
+
+  aleph::topology::io::AdjacencyMatrixReader reader;
+
+  // Simple ------------------------------------------------------------
+
+  SimplicialComplex K;
+
+  stream.str( inputSimple );
+  reader( stream, K );
+
+  ALEPH_ASSERT_THROW( K.empty() == false );
+  ALEPH_ASSERT_EQUAL( K.size(), 3 + 3 );    // 3 nodes, 3 edges
+
+  std::vector<DataType> weights;
+
+  std::transform( K.begin(), K.end(), std::back_inserter( weights ),
+    [] ( const Simplex& s )
+    {
+      return s.data();
+    }
+  );
+
+  std::sort( weights.begin(), weights.end() );
+
+  std::vector<DataType> expectedWeights = {
+    0,0,0, // vertices
+    1,3,5  // edges (sorted)
+  };
+
+  ALEPH_ASSERT_THROW( weights == expectedWeights );
+
+  // Broken ------------------------------------------------------------
+
+  SimplicialComplex L;
+
+  stream.clear();
+  stream.str( inputBroken );
+
+  try
+  {
+    reader( stream, L );
+  }
+  catch( std::runtime_error& )
+  {
+  }
+
+  ALEPH_ASSERT_THROW( L.empty() );
+
+  ALEPH_TEST_END();
+}
+
+int main( int, char** )
+{
+  testSimple<float> ();
+  testSimple<double>();
+}
